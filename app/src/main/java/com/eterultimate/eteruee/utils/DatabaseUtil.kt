588a4eee8a53ae1804018dbcf1abe5ea9e46dac5
EterUltimate <1831303476@qq.com>
2026-05-11 09:25:43 +0800

    fix: 移除无用的反射修改避免报错

--- app/src/main/java/com/eterultimate/eteruee/utils/DatabaseUtil.kt
+++ app/src/main/java/com/eterultimate/eteruee/utils/DatabaseUtil.kt
@@ -16,16 +16,17 @@ object DatabaseUtil {
         } catch (e: Exception) {
             e.printStackTrace()
         }
-        try {
-            val field =
-                io.requery.android.database.CursorWindow::class.java.getDeclaredField("sDefaultCursorWindowSize")
-            field.isAccessible = true
-            val oldValue = field.get(null) as Int
-            field.set(null, size)
-            Log.i(TAG, "setCursorWindowSize: set $oldValue to $size")
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        // 宸瞗ork io.requery.android.database 淇敼浜唚indow size锛岄伩鍏嶆棤娉曞弽灏勪慨鏀筬inal瀛楁
+//        try {
+//            val field =
+//                io.requery.android.database.CursorWindow::class.java.getDeclaredField("sDefaultCursorWindowSize")
+//            field.isAccessible = true
+//            val oldValue = field.get(null) as Int
+//            field.set(null, size)
+//            Log.i(TAG, "setCursorWindowSize: set $oldValue to $size")
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
     }
 }
 

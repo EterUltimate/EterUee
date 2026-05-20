rikkahub/rikkahub

feat: 新增自定义配色功能
支持用户通过种子色创建自定义主题，使用 HSL 色彩空间生成完整 Material 3 配色方案。
新增主题管理页面，可创建/编辑/删除自定义主题，并将主题入口移至显示设置页面。

app/src/main/java/me/rerere/rikkahub/RouteActivity.kt
@@ -100,6 +100,7 @@ import me.rerere.rikkahub.ui.pages.log.LogPage
 import me.rerere.rikkahub.ui.pages.search.SearchPage
 import me.rerere.rikkahub.ui.pages.setting.SettingAboutPage
 import me.rerere.rikkahub.ui.pages.setting.SettingDisplayPage
+import me.rerere.rikkahub.ui.pages.setting.SettingThemePage
 import me.rerere.rikkahub.ui.pages.setting.SettingDonatePage
 import me.rerere.rikkahub.ui.pages.setting.SettingFilesPage
 import me.rerere.rikkahub.ui.pages.setting.SettingMcpPage
@@ -382,6 +383,10 @@ class RouteActivity : ComponentActivity() {
                                 WebViewPage(key.url, key.content)
                             }
 
+                            entry<Screen.SettingTheme> {
+                                SettingThemePage()
+                            }
+
                             entry<Screen.SettingDisplay> {
                                 SettingDisplayPage()
                             }
@@ -580,6 +585,9 @@ sealed interface Screen : NavKey {
 
     @Serializable
     data class WebView(val url: String = "", val content: String = "") : Screen
+
+    @Serializable
+    data object SettingTheme : Screen
 
     @Serializable
     data object SettingDisplay : Screen

app/src/main/java/me/rerere/rikkahub/data/datastore/PreferencesStore.kt
@@ -41,6 +41,7 @@ import me.rerere.rikkahub.data.model.PromptInjection
 import me.rerere.rikkahub.data.model.QuickMessage
 import me.rerere.rikkahub.data.model.Tag
 import me.rerere.rikkahub.data.sync.s3.S3Config
+import me.rerere.rikkahub.ui.theme.CustomTheme
 import me.rerere.rikkahub.ui.theme.PresetThemes
 import me.rerere.rikkahub.utils.JsonInstant
 import me.rerere.rikkahub.utils.toMutableStateFlow
@@ -75,6 +76,7 @@ class SettingsStore(
         // UI设置
         val DYNAMIC_COLOR = booleanPreferencesKey("dynamic_color")
         val THEME_ID = stringPreferencesKey("theme_id")
+        val CUSTOM_THEMES = stringPreferencesKey("custom_themes")
         val DISPLAY_SETTING = stringPreferencesKey("display_setting")
         val DEVELOPER_MODE = booleanPreferencesKey("developer_mode")
 
@@ -188,6 +190,9 @@ class SettingsStore(
                 assistants = JsonInstant.decodeFromString(preferences[ASSISTANTS] ?: "[]"),
                 dynamicColor = preferences[DYNAMIC_COLOR] != false,
                 themeId = preferences[THEME_ID] ?: PresetThemes[0].id,
+                customThemes = preferences[CUSTOM_THEMES]?.let {
+                    JsonInstant.decodeFromString(it)
+                } ?: emptyList(),
                 developerMode = preferences[DEVELOPER_MODE] == true,
                 displaySetting = JsonInstant.decodeFromString(preferences[DISPLAY_SETTING] ?: "{}"),
                 searchServices = preferences[SEARCH_SERVICES]?.let {
@@ -344,6 +349,7 @@ class SettingsStore(
         dataStore.edit { preferences ->
             preferences[DYNAMIC_COLOR] = settings.dynamicColor
             preferences[THEME_ID] = settings.themeId
+            preferences[CUSTOM_THEMES] = JsonInstant.encodeToString(settings.customThemes)
             preferences[DEVELOPER_MODE] = settings.developerMode
             preferences[DISPLAY_SETTING] = JsonInstant.encodeToString(settings.displaySetting)
 
@@ -480,6 +486,7 @@ data class Settings(
     val init: Boolean = false,
     val dynamicColor: Boolean = true,
     val themeId: String = PresetThemes[0].id,
+    val customThemes: List<CustomTheme> = emptyList(),
     val developerMode: Boolean = false,
     val displaySetting: DisplaySetting = DisplaySetting(),
     val enableWebSearch: Boolean = false,

app/src/main/java/me/rerere/rikkahub/ui/pages/setting/components/CustomThemeButton.kt
@@ -1,0 +1,124 @@
+package me.rerere.rikkahub.ui.pages.setting.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import me.rerere.hugeicons.HugeIcons
+import me.rerere.hugeicons.stroke.Tick01
+import me.rerere.rikkahub.ui.theme.CustomTheme
+import me.rerere.rikkahub.ui.theme.LocalDarkMode
+
+@Composable
+fun CustomThemeButtonGroup(
+    themes: List<CustomTheme>,
+    selectedThemeId: String,
+    modifier: Modifier = Modifier,
+    onChangeTheme: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            themes.fastForEach { theme ->
+                CustomThemeButton(
+                    theme = theme,
+                    selected = theme.id == selectedThemeId,
+                    onClick = { onChangeTheme(theme.id) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CustomThemeButton(
+    theme: CustomTheme,
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val darkMode = LocalDarkMode.current
+    val scheme = theme.generateColorScheme(darkMode)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = LocalIndication.current,
+                onClick = onClick
+            )
+            .padding(8.dp),
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Canvas(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(48.dp)
+            ) {
+                drawRect(color = scheme.primaryContainer, size = size)
+                drawRect(
+                    color = scheme.secondaryContainer,
+                    size = size,
+                    topLeft = Offset(x = size.width / 2, y = 0f),
+                )
+                drawRect(
+                    color = scheme.tertiaryContainer,
+                    size = size,
+                    topLeft = Offset(x = size.width / 2, y = size.height / 2),
+                )
+                drawCircle(
+                    color = scheme.primary,
+                    radius = if (selected) 12.dp.toPx() else 8.dp.toPx(),
+                    center = Offset(x = size.width / 2, y = size.height / 2)
+                )
+            }
+            if (selected) {
+                Icon(
+                    HugeIcons.Tick01,
+                    contentDescription = null,
+                    tint = scheme.onPrimary
+                )
+            }
+        }
+        Text(
+            text = theme.name.ifEmpty { "?" },
+            style = MaterialTheme.typography.labelMedium.copy(color = scheme.primary),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}

EterUltimate/EterUee

app/src/main/java/com/eterultimate/eteruee/RouteActivity.kt
@@ -107,6 +107,7 @@ import com.eterultimate.eteruee.ui.pages.setting.SettingProviderDetailPage
 import com.eterultimate.eteruee.ui.pages.setting.SettingProviderPage
 import com.eterultimate.eteruee.ui.pages.setting.SettingSearchDetailPage
 import com.eterultimate.eteruee.ui.pages.setting.SettingSearchPage
+import com.eterultimate.eteruee.ui.pages.setting.SettingThemePage
 import com.eterultimate.eteruee.ui.pages.setting.SettingTTSPage
 import com.eterultimate.eteruee.ui.pages.setting.SettingWebPage
 import com.eterultimate.eteruee.ui.pages.share.handler.ShareHandlerPage
@@ -387,6 +388,10 @@ class RouteActivity : ComponentActivity() {
                                 WebViewPage(key.url, key.content)
                             }
 
+                            entry<Screen.SettingTheme> {
+                                SettingThemePage()
+                            }
+
                             entry<Screen.SettingDisplay> {
                                 SettingDisplayPage()
                             }
@@ -662,6 +667,9 @@ sealed interface Screen : NavKey {
 
     @Serializable
     data class WebView(val url: String = "", val content: String = "") : Screen
+
+    @Serializable
+    data object SettingTheme : Screen
 
     @Serializable
     data object SettingDisplay : Screen

app/src/main/java/com/eterultimate/eteruee/data/datastore/PreferencesStore.kt
@@ -40,6 +40,7 @@ import com.eterultimate.eteruee.data.model.PromptInjection
 import com.eterultimate.eteruee.data.model.QuickMessage
 import com.eterultimate.eteruee.data.model.Tag
 import com.eterultimate.eteruee.data.sync.s3.S3Config
+import com.eterultimate.eteruee.ui.theme.CustomTheme
 import com.eterultimate.eteruee.ui.theme.PresetThemes
 import com.eterultimate.eteruee.utils.JsonInstant
 import com.eterultimate.eteruee.utils.toMutableStateFlow
@@ -74,6 +75,7 @@ class SettingsStore(
         // UI设置
         val DYNAMIC_COLOR = booleanPreferencesKey("dynamic_color")
         val THEME_ID = stringPreferencesKey("theme_id")
+        val CUSTOM_THEMES = stringPreferencesKey("custom_themes")
         val DISPLAY_SETTING = stringPreferencesKey("display_setting")
         val DEVELOPER_MODE = booleanPreferencesKey("developer_mode")
 
@@ -185,6 +187,9 @@ class SettingsStore(
                 assistants = JsonInstant.decodeFromString(preferences[ASSISTANTS] ?: "[]"),
                 dynamicColor = preferences[DYNAMIC_COLOR] != false,
                 themeId = preferences[THEME_ID] ?: PresetThemes[0].id,
+                customThemes = preferences[CUSTOM_THEMES]?.let {
+                    JsonInstant.decodeFromString(it)
+                } ?: emptyList(),
                 developerMode = preferences[DEVELOPER_MODE] == true,
                 displaySetting = JsonInstant.decodeFromString(preferences[DISPLAY_SETTING] ?: "{}"),
                 searchServices = preferences[SEARCH_SERVICES]?.let {
@@ -332,6 +337,7 @@ class SettingsStore(
         dataStore.edit { preferences ->
             preferences[DYNAMIC_COLOR] = settings.dynamicColor
             preferences[THEME_ID] = settings.themeId
+            preferences[CUSTOM_THEMES] = JsonInstant.encodeToString(settings.customThemes)
             preferences[DEVELOPER_MODE] = settings.developerMode
             preferences[DISPLAY_SETTING] = JsonInstant.encodeToString(settings.displaySetting)
 
@@ -465,6 +471,7 @@ data class Settings(
     val init: Boolean = false,
     val dynamicColor: Boolean = true,
     val themeId: String = PresetThemes[0].id,
+    val customThemes: List<CustomTheme> = emptyList(),
     val developerMode: Boolean = false,
     val displaySetting: DisplaySetting = DisplaySetting(),
     val enableWebSearch: Boolean = false,

app/src/main/java/com/eterultimate/eteruee/ui/pages/setting/components/CustomThemeButton.kt
@@ -1,0 +1,124 @@
+package com.eterultimate.eteruee.ui.pages.setting.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import me.rerere.hugeicons.HugeIcons
+import me.rerere.hugeicons.stroke.Tick01
+import com.eterultimate.eteruee.ui.theme.CustomTheme
+import com.eterultimate.eteruee.ui.theme.LocalDarkMode
+
+@Composable
+fun CustomThemeButtonGroup(
+    themes: List<CustomTheme>,
+    selectedThemeId: String,
+    modifier: Modifier = Modifier,
+    onChangeTheme: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            themes.fastForEach { theme ->
+                CustomThemeButton(
+                    theme = theme,
+                    selected = theme.id == selectedThemeId,
+                    onClick = { onChangeTheme(theme.id) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CustomThemeButton(
+    theme: CustomTheme,
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val darkMode = LocalDarkMode.current
+    val scheme = theme.generateColorScheme(darkMode)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = LocalIndication.current,
+                onClick = onClick
+            )
+            .padding(8.dp),
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Canvas(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(48.dp)
+            ) {
+                drawRect(color = scheme.primaryContainer, size = size)
+                drawRect(
+                    color = scheme.secondaryContainer,
+                    size = size,
+                    topLeft = Offset(x = size.width / 2, y = 0f),
+                )
+                drawRect(
+                    color = scheme.tertiaryContainer,
+                    size = size,
+                    topLeft = Offset(x = size.width / 2, y = size.height / 2),
+                )
+                drawCircle(
+                    color = scheme.primary,
+                    radius = if (selected) 12.dp.toPx() else 8.dp.toPx(),
+                    center = Offset(x = size.width / 2, y = size.height / 2)
+                )
+            }
+            if (selected) {
+                Icon(
+                    HugeIcons.Tick01,
+                    contentDescription = null,
+                    tint = scheme.onPrimary
+                )
+            }
+        }
+        Text(
+            text = theme.name.ifEmpty { "?" },
+            style = MaterialTheme.typography.labelMedium.copy(color = scheme.primary),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
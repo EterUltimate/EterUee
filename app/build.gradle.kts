3bc30ba90023355d1e92e495f26b2fe18e44b84e
EterUltimate <1831303476@qq.com>
2026-05-29 08:09:21 +0800

    chore: release 5.2.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.eterultimate.eteruee"
         minSdk = 26
         targetSdk = 37
-        versionCode = 165
-        versionName = "5.2.10"
+        versionCode = 166
+        versionName = "5.2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

rikkahub/rikkahub

feat: 将最近聊天引用改为按需调用的对话工具
把 enableRecentChatsReference 从静态注入 system prompt 改为向 AI 注册
recent_chats 和 conversation_search 两个工具，避免动态内容破坏 prompt 缓存。
同时补充工具的展示 UI 与多语言文案。

app/src/main/java/me/rerere/rikkahub/data/ai/tools/ConversationTools.kt
@@ -1,0 +1,111 @@
+package me.rerere.rikkahub.data.ai.tools
+
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.intOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
+import me.rerere.ai.core.InputSchema
+import me.rerere.ai.core.Tool
+import me.rerere.ai.ui.UIMessagePart
+import me.rerere.rikkahub.data.db.fts.MessageSearchSort
+import me.rerere.rikkahub.data.repository.ConversationRepository
+import me.rerere.rikkahub.utils.JsonInstantPretty
+import me.rerere.rikkahub.utils.toLocalDate
+import kotlin.uuid.Uuid
+
+/**
+ * Tools that let the assistant query the user's past conversations on demand, instead of
+ * statically injecting recent chats into the system prompt (which would break prompt caching).
+ */
+fun createConversationTools(
+    conversationRepo: ConversationRepository,
+    assistantId: Uuid,
+): List<Tool> = listOf(
+    Tool(
+        name = "recent_chats",
+        description = """
+            List the user's recent conversations with you to understand their preferences and ongoing topics.
+            Returns conversation titles and the date of last activity, ordered by pinned first then most recently updated.
+            Use this when you need quick context about what the user has been discussing lately.
+            Only titles and dates are returned; use `conversation_search` to look up the actual content.
+        """.trimIndent(),
+        parameters = {
+            InputSchema.Obj(
+                properties = buildJsonObject {
+                    put("limit", buildJsonObject {
+                        put("type", "integer")
+                        put(
+                            "description",
+                            "Maximum number of recent conversations to return (default: 10, max: 30)"
+                        )
+                    })
+                }
+            )
+        },
+        execute = {
+            val limit = (it.jsonObject["limit"]?.jsonPrimitive?.intOrNull ?: 10).coerceIn(1, 30)
+            val recent = conversationRepo.getRecentConversations(
+                assistantId = assistantId,
+                limit = limit,
+            )
+            val payload = buildJsonArray {
+                recent.forEach { conversation ->
+                    add(buildJsonObject {
+                        put("id", conversation.id.toString())
+                        put("title", conversation.title.ifBlank { "Untitled" })
+                        put("last_chat", conversation.updateAt.toLocalDate())
+                    })
+                }
+            }
+            listOf(UIMessagePart.Text(JsonInstantPretty.encodeToString(payload)))
+        }
+    ),
+    Tool(
+        name = "conversation_search",
+        description = """
+            Full-text search across the user's past conversations to recall specific information they mentioned before.
+            Use focused keywords. Run multiple searches with different keywords if needed.
+            Each result includes the conversation title, a snippet with matched keywords wrapped in [brackets], and the date.
+        """.trimIndent(),
+        parameters = {
+            InputSchema.Obj(
+                properties = buildJsonObject {
+                    put("query", buildJsonObject {
+                        put("type", "string")
+                        put("description", "Keywords to search for in past conversation messages")
+                    })
+                    put("limit", buildJsonObject {
+                        put("type", "integer")
+                        put(
+                            "description",
+                            "Maximum number of results to return (default: 15, max: 50)"
+                        )
+                    })
+                },
+                required = listOf("query")
+            )
+        },
+        execute = {
+            val query = it.jsonObject["query"]?.jsonPrimitive?.contentOrNull
+                ?: error("query is required")
+            val limit = (it.jsonObject["limit"]?.jsonPrimitive?.intOrNull ?: 15).coerceIn(1, 50)
+            val results = conversationRepo
+                .searchMessages(query, MessageSearchSort.RELEVANCE)
+                .take(limit)
+            val payload = buildJsonArray {
+                results.forEach { result ->
+                    add(buildJsonObject {
+                        put("conversation_id", result.conversationId)
+                        put("title", result.title.ifBlank { "Untitled" })
+                        put("snippet", result.snippet)
+                        put("date", result.updateAt.toLocalDate())
+                    })
+                }
+            }
+            listOf(UIMessagePart.Text(JsonInstantPretty.encodeToString(payload)))
+        }
+    )
+)

app/src/main/java/me/rerere/rikkahub/service/ChatService.kt
@@ -56,6 +56,7 @@ import me.rerere.rikkahub.data.ai.GenerationChunk
 import me.rerere.rikkahub.data.ai.GenerationHandler
 import me.rerere.rikkahub.data.ai.mcp.McpManager
 import me.rerere.rikkahub.data.ai.tools.LocalTools
+import me.rerere.rikkahub.data.ai.tools.createConversationTools
 import me.rerere.rikkahub.data.ai.tools.createSearchTools
 import me.rerere.rikkahub.data.ai.tools.createSkillTools
 import me.rerere.rikkahub.data.ai.tools.createWorkspaceTools
@@ -557,6 +558,9 @@ class ChatService(
                         addAll(createSearchTools(settings))
                     }
                     addAll(localTools.getTools(assistant.localTools))
+                    if (assistant.enableRecentChatsReference) {
+                        addAll(createConversationTools(conversationRepo, assistant.id))
+                    }
                     addAll(createWorkspaceToolsIfReady(assistant.workspaceId?.toString(), conversation.workspaceCwd))
                     if (assistant.enabledSkills.isNotEmpty()) {
                         addAll(

EterUltimate/EterUee

app/src/main/java/com/eterultimate/eteruee/data/ai/tools/ConversationTools.kt
@@ -1,0 +1,110 @@
+package com.eterultimate.eteruee.data.ai.tools
+
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.intOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
+import com.eterultimate.eteruee.ai.core.InputSchema
+import com.eterultimate.eteruee.ai.core.Tool
+import com.eterultimate.eteruee.ai.ui.UIMessagePart
+import com.eterultimate.eteruee.data.repository.ConversationRepository
+import com.eterultimate.eteruee.utils.JsonInstantPretty
+import com.eterultimate.eteruee.utils.toLocalDate
+import kotlin.uuid.Uuid
+
+/**
+ * Tools that let the assistant query the user's past conversations on demand, instead of
+ * statically injecting recent chats into the system prompt (which would break prompt caching).
+ */
+fun createConversationTools(
+    conversationRepo: ConversationRepository,
+    assistantId: Uuid,
+): List<Tool> = listOf(
+    Tool(
+        name = "recent_chats",
+        description = """
+            List the user's recent conversations with you to understand their preferences and ongoing topics.
+            Returns conversation titles and the date of last activity, ordered by pinned first then most recently updated.
+            Use this when you need quick context about what the user has been discussing lately.
+            Only titles and dates are returned; use `conversation_search` to look up the actual content.
+        """.trimIndent(),
+        parameters = {
+            InputSchema.Obj(
+                properties = buildJsonObject {
+                    put("limit", buildJsonObject {
+                        put("type", "integer")
+                        put(
+                            "description",
+                            "Maximum number of recent conversations to return (default: 10, max: 30)"
+                        )
+                    })
+                }
+            )
+        },
+        execute = {
+            val limit = (it.jsonObject["limit"]?.jsonPrimitive?.intOrNull ?: 10).coerceIn(1, 30)
+            val recent = conversationRepo.getRecentConversations(
+                assistantId = assistantId,
+                limit = limit,
+            )
+            val payload = buildJsonArray {
+                recent.forEach { conversation ->
+                    add(buildJsonObject {
+                        put("id", conversation.id.toString())
+                        put("title", conversation.title.ifBlank { "Untitled" })
+                        put("last_chat", conversation.updateAt.toLocalDate())
+                    })
+                }
+            }
+            listOf(UIMessagePart.Text(JsonInstantPretty.encodeToString(payload)))
+        }
+    ),
+    Tool(
+        name = "conversation_search",
+        description = """
+            Full-text search across the user's past conversations to recall specific information they mentioned before.
+            Use focused keywords. Run multiple searches with different keywords if needed.
+            Each result includes the conversation title, a snippet with matched keywords wrapped in [brackets], and the date.
+        """.trimIndent(),
+        parameters = {
+            InputSchema.Obj(
+                properties = buildJsonObject {
+                    put("query", buildJsonObject {
+                        put("type", "string")
+                        put("description", "Keywords to search for in past conversation messages")
+                    })
+                    put("limit", buildJsonObject {
+                        put("type", "integer")
+                        put(
+                            "description",
+                            "Maximum number of results to return (default: 15, max: 50)"
+                        )
+                    })
+                },
+                required = listOf("query")
+            )
+        },
+        execute = {
+            val query = it.jsonObject["query"]?.jsonPrimitive?.contentOrNull
+                ?: error("query is required")
+            val limit = (it.jsonObject["limit"]?.jsonPrimitive?.intOrNull ?: 15).coerceIn(1, 50)
+            val results = conversationRepo
+                .searchMessages(query)
+                .take(limit)
+            val payload = buildJsonArray {
+                results.forEach { result ->
+                    add(buildJsonObject {
+                        put("conversation_id", result.conversationId)
+                        put("title", result.title.ifBlank { "Untitled" })
+                        put("snippet", result.snippet)
+                        put("date", result.updateAt.toLocalDate())
+                    })
+                }
+            }
+            listOf(UIMessagePart.Text(JsonInstantPretty.encodeToString(payload)))
+        }
+    )
+)

app/src/main/java/com/eterultimate/eteruee/service/ChatService.kt
@@ -56,6 +56,7 @@ import com.eterultimate.eteruee.data.ai.GenerationChunk
 import com.eterultimate.eteruee.data.ai.GenerationHandler
 import com.eterultimate.eteruee.data.ai.mcp.McpManager
 import com.eterultimate.eteruee.data.ai.tools.LocalTools
+import com.eterultimate.eteruee.data.ai.tools.createConversationTools
 import com.eterultimate.eteruee.data.ai.tools.createSearchTools
 import com.eterultimate.eteruee.data.ai.tools.createSkillTools
 import com.eterultimate.eteruee.data.files.SkillManager
@@ -550,6 +551,9 @@ class ChatService(
                         addAll(createSearchTools(settings))
                     }
                     addAll(localTools.getTools(assistant.localTools))
+                    if (assistant.enableRecentChatsReference) {
+                        addAll(createConversationTools(conversationRepo, assistant.id))
+                    }
                     if (assistant.enabledSkills.isNotEmpty()) {
                         addAll(
                             createSkillTools(
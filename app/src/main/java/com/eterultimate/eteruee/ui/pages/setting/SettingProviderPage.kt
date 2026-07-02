rikkahub/rikkahub

fix: SettingProviderPage 适配沉浸式导航栏
LazyColumn 延伸到屏幕底边并将系统导航栏 inset 加入 contentPadding，
使列表内容可滚动到导航栏之后且最后一项不被遮挡。

Closes #1330

app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingProviderPage.kt
@@ -79,6 +79,7 @@ import me.rerere.rikkahub.ui.hooks.useEditState
 import me.rerere.rikkahub.ui.pages.setting.components.ProviderConfigure
 import me.rerere.rikkahub.ui.theme.CustomColors
 import me.rerere.rikkahub.utils.ImageUtils
+import me.rerere.rikkahub.utils.plus
 import org.koin.androidx.compose.koinViewModel
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
@@ -162,7 +163,7 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(top = innerPadding.calculateTopPadding())
         ) {
             // Search bar
             OutlinedTextField(
@@ -192,7 +193,8 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                     .fillMaxWidth()
                     .weight(1f)
                     .imePadding(),
-                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp) +
+                    PaddingValues(bottom = innerPadding.calculateBottomPadding()),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 state = lazyListState,
             ) {

EterUltimate/EterUee

(cherry picked from commit 37db4736912a6177f0cc24720d407b48c0d8fb2e)

app/src/main/java/com/eterultimate/eteruee/ui/pages/setting/SettingProviderPage.kt
@@ -80,6 +80,7 @@ import com.eterultimate.eteruee.ui.hooks.useEditState
 import com.eterultimate.eteruee.ui.pages.setting.components.ProviderConfigure
 import com.eterultimate.eteruee.ui.theme.CustomColors
 import com.eterultimate.eteruee.utils.ImageUtils
+import com.eterultimate.eteruee.utils.plus
 import org.koin.androidx.compose.koinViewModel
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyStaggeredGridState
@@ -163,7 +164,7 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(top = innerPadding.calculateTopPadding())
         ) {
             // Search bar
             OutlinedTextField(
@@ -193,7 +194,8 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                     .fillMaxWidth()
                     .weight(1f)
                     .imePadding(),
-                contentPadding = PaddingValues(16.dp),
+                contentPadding = PaddingValues(16.dp) +
+                    PaddingValues(bottom = innerPadding.calculateBottomPadding()),
                 verticalItemSpacing = 8.dp,
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 state = lazyListState,
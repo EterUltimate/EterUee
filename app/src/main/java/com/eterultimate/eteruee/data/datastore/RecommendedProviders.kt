rikkahub/rikkahub

feat(provider): 新增推荐提供商 Sheet 入口
将提供商设置页右上角的 AiHubMix 跳转按钮改为推荐提供商底部弹窗，
可通过 RECOMMENDED_PROVIDERS 配置展示，点击加号即可添加到列表。
同时将 README 赞助商部分改为表格以便扩展。

app/src/main/java/me/rerere/rikkahub/data/datastore/RecommendedProviders.kt
@@ -1,0 +1,46 @@
+package me.rerere.rikkahub.data.datastore
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
+import me.rerere.ai.provider.ProviderSetting
+import me.rerere.rikkahub.ui.components.richtext.MarkdownBlock
+import kotlin.uuid.Uuid
+
+/**
+ * 推荐的提供商列表，在提供商设置页右上角的推荐 Sheet 中展示。
+ */
+val RECOMMENDED_PROVIDERS: List<ProviderSetting> = listOf(
+    ProviderSetting.OpenAI(
+        id = Uuid.parse("1b1395ed-b702-4aeb-8bc1-b681c4456953"),
+        name = "AiHubMix",
+        baseUrl = "https://aihubmix.com/v1",
+        apiKey = "",
+        enabled = true,
+        description = {
+            Text(
+                text = buildAnnotatedString {
+                    append("提供 OpenAI、Claude、Google Gemini 等主流模型的高并发和稳定服务")
+                    appendLine()
+                    append("官网：")
+                    withLink(LinkAnnotation.Url("https://aihubmix.com?aff=pG7r")) {
+                        withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                            append("https://aihubmix.com")
+                        }
+                    }
+                    appendLine()
+                    append("充值: ")
+                    withLink(LinkAnnotation.Url("https://console.aihubmix.com/topup")) {
+                        withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                            append("https://console.aihubmix.com/topup")
+                        }
+                    }
+                }
+            )
+        },
+    ),
+)

app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingProviderPage.kt
@@ -8,6 +8,7 @@ import me.rerere.hugeicons.stroke.Image02
 import me.rerere.hugeicons.stroke.FileImport
 import me.rerere.hugeicons.stroke.Add01
 import me.rerere.hugeicons.stroke.Search01
+import me.rerere.hugeicons.stroke.Sparkles
 import me.rerere.hugeicons.stroke.Cancel01
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -37,19 +38,21 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFlexibleTopAppBar
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -68,6 +71,7 @@ import io.github.g00fy2.quickie.ScanQRCode
 import me.rerere.ai.provider.ProviderSetting
 import me.rerere.rikkahub.R
 import me.rerere.rikkahub.Screen
+import me.rerere.rikkahub.data.datastore.RECOMMENDED_PROVIDERS
 import me.rerere.rikkahub.ui.components.nav.BackButton
 import me.rerere.rikkahub.ui.components.ui.AutoAIIcon
 import me.rerere.rikkahub.ui.components.ui.Tag
@@ -83,15 +87,12 @@ import me.rerere.rikkahub.utils.plus
 import org.koin.androidx.compose.koinViewModel
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
-import java.util.Locale
-import kotlinx.coroutines.launch
 import kotlin.uuid.Uuid
 
 @Composable
 fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
     val settings by vm.settings.collectAsStateWithLifecycle()
     val navController = LocalNavController.current
-    val scope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     var searchQuery by remember { mutableStateOf("") }
     val lazyListState = rememberLazyListState()
@@ -122,21 +123,12 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                     BackButton()
                 },
                 actions = {
-                    if(Locale.getDefault().language == "zh") {
-                        IconButton(
-                            onClick = {
-                                val aihubmixIndex = filteredProviders.indexOfFirst {
-                                    it.id.toString() == "1b1395ed-b702-4aeb-8bc1-b681c4456953"
-                                }
-                                if (aihubmixIndex != -1) {
-                                    scope.launch {
-                                        lazyListState.animateScrollToItem(aihubmixIndex)
-                                    }
-                                }
-                            }
-                        ) {
-                            AutoAIIcon("AiHubMix")
-                        }
+                    RecommendProviderButton { provider ->
+                        vm.updateSettings(
+                            settings.copy(
+                                providers = listOf(provider.copyProvider(Uuid.random())) + settings.providers
+                            )
+                        )
                     }
                     ImportProviderButton {
                         vm.updateSettings(
@@ -234,6 +226,101 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                         )
                     }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecommendProviderButton(
+    onAdd: (ProviderSetting) -> Unit
+) {
+    val toaster = LocalToaster.current
+    var showSheet by remember { mutableStateOf(false) }
+    val importSuccessMessage = stringResource(R.string.setting_provider_page_import_success)
+
+    IconButton(
+        onClick = { showSheet = true }
+    ) {
+        Icon(HugeIcons.Sparkles, contentDescription = stringResource(R.string.setting_provider_page_recommend))
+    }
+
+    if (showSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showSheet = false },
+            sheetState = rememberBottomSheetState(
+                initialValue = SheetValue.Expanded,
+                enabledValues = setOf(SheetValue.Hidden, SheetValue.Expanded)
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.setting_provider_page_recommend),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                RECOMMENDED_PROVIDERS.forEach { provider ->
+                    RecommendProviderItem(
+                        provider = provider,
+                        onAdd = {
+                            onAdd(provider)
+                            toaster.show(
+                                importSuccessMessage,
+                                type = ToastType.Success
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecommendProviderItem(
+    provider: ProviderSetting,
+    onAdd: () -> Unit
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = CustomColors.listItemColors.containerColor
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AutoAIIcon(
+                name = provider.name,
+                modifier = Modifier.size(40.dp)
+            )
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                Text(
+                    text = provider.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                ProvideTextStyle(MaterialTheme.typography.labelSmall) {
+                    CompositionLocalProvider(LocalContentColor provides LocalContentColor.current.copy(alpha = 0.7f)) {
+                        provider.description()
+                    }
+                }
+            }
+            IconButton(onClick = onAdd) {
+                Icon(HugeIcons.Add01, contentDescription = stringResource(R.string.setting_provider_page_add))
             }
         }
     }

EterUltimate/EterUee

app/src/main/java/com/eterultimate/eteruee/data/datastore/RecommendedProviders.kt
@@ -1,0 +1,45 @@
+package com.eterultimate.eteruee.data.datastore
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
+import com.eterultimate.eteruee.ai.provider.ProviderSetting
+import kotlin.uuid.Uuid
+
+/**
+ * 推荐的提供商列表，在提供商设置页右上角的推荐 Sheet 中展示。
+ */
+val RECOMMENDED_PROVIDERS: List<ProviderSetting> = listOf(
+    ProviderSetting.OpenAI(
+        id = Uuid.parse("1b1395ed-b702-4aeb-8bc1-b681c4456953"),
+        name = "AiHubMix",
+        baseUrl = "https://aihubmix.com/v1",
+        apiKey = "",
+        enabled = true,
+        description = {
+            Text(
+                text = buildAnnotatedString {
+                    append("提供 OpenAI、Claude、Google Gemini 等主流模型的高并发和稳定服务")
+                    appendLine()
+                    append("官网：")
+                    withLink(LinkAnnotation.Url("https://aihubmix.com?aff=pG7r")) {
+                        withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                            append("https://aihubmix.com")
+                        }
+                    }
+                    appendLine()
+                    append("充值: ")
+                    withLink(LinkAnnotation.Url("https://console.aihubmix.com/topup")) {
+                        withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                            append("https://console.aihubmix.com/topup")
+                        }
+                    }
+                }
+            )
+        },
+    ),
+)

app/src/main/java/com/eterultimate/eteruee/ui/pages/setting/SettingProviderPage.kt
@@ -9,6 +9,7 @@ import me.rerere.hugeicons.stroke.Image02
 import me.rerere.hugeicons.stroke.FileImport
 import me.rerere.hugeicons.stroke.Add01
 import me.rerere.hugeicons.stroke.Search01
+import me.rerere.hugeicons.stroke.Sparkles
 import me.rerere.hugeicons.stroke.Cancel01
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -38,19 +39,21 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFlexibleTopAppBar
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -69,6 +72,7 @@ import io.github.g00fy2.quickie.ScanQRCode
 import com.eterultimate.eteruee.ai.provider.ProviderSetting
 import com.eterultimate.eteruee.R
 import com.eterultimate.eteruee.Screen
+import com.eterultimate.eteruee.data.datastore.RECOMMENDED_PROVIDERS
 import com.eterultimate.eteruee.ui.components.nav.BackButton
 import com.eterultimate.eteruee.ui.components.ui.AutoAIIcon
 import com.eterultimate.eteruee.ui.components.ui.Tag
@@ -84,15 +88,12 @@ import com.eterultimate.eteruee.utils.plus
 import org.koin.androidx.compose.koinViewModel
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyStaggeredGridState
-import java.util.Locale
-import kotlinx.coroutines.launch
 import kotlin.uuid.Uuid
 
 @Composable
 fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
     val settings by vm.settings.collectAsStateWithLifecycle()
     val navController = LocalNavController.current
-    val scope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     var searchQuery by remember { mutableStateOf("") }
     val lazyListState = rememberLazyStaggeredGridState()
@@ -123,21 +124,12 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                     BackButton()
                 },
                 actions = {
-                    if(Locale.getDefault().language == "zh") {
-                        IconButton(
-                            onClick = {
-                                val aihubmixIndex = filteredProviders.indexOfFirst {
-                                    it.id.toString() == "1b1395ed-b702-4aeb-8bc1-b681c4456953"
-                                }
-                                if (aihubmixIndex != -1) {
-                                    scope.launch {
-                                        lazyListState.animateScrollToItem(aihubmixIndex)
-                                    }
-                                }
-                            }
-                        ) {
-                            AutoAIIcon("AiHubMix")
-                        }
+                    RecommendProviderButton { provider ->
+                        vm.updateSettings(
+                            settings.copy(
+                                providers = listOf(provider.copyProvider(Uuid.random())) + settings.providers
+                            )
+                        )
                     }
                     ImportProviderButton {
                         vm.updateSettings(
@@ -237,6 +229,101 @@ fun SettingProviderPage(vm: SettingVM = koinViewModel()) {
                         )
                     }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecommendProviderButton(
+    onAdd: (ProviderSetting) -> Unit
+) {
+    val toaster = LocalToaster.current
+    var showSheet by remember { mutableStateOf(false) }
+    val importSuccessMessage = stringResource(R.string.setting_provider_page_import_success)
+
+    IconButton(
+        onClick = { showSheet = true }
+    ) {
+        Icon(HugeIcons.Sparkles, contentDescription = stringResource(R.string.setting_provider_page_recommend))
+    }
+
+    if (showSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showSheet = false },
+            sheetState = rememberBottomSheetState(
+                initialValue = SheetValue.Expanded,
+                enabledValues = setOf(SheetValue.Hidden, SheetValue.Expanded)
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.setting_provider_page_recommend),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                RECOMMENDED_PROVIDERS.forEach { provider ->
+                    RecommendProviderItem(
+                        provider = provider,
+                        onAdd = {
+                            onAdd(provider)
+                            toaster.show(
+                                importSuccessMessage,
+                                type = ToastType.Success
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecommendProviderItem(
+    provider: ProviderSetting,
+    onAdd: () -> Unit
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = CustomColors.listItemColors.containerColor
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AutoAIIcon(
+                name = provider.name,
+                modifier = Modifier.size(40.dp)
+            )
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                Text(
+                    text = provider.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                ProvideTextStyle(MaterialTheme.typography.labelSmall) {
+                    CompositionLocalProvider(LocalContentColor provides LocalContentColor.current.copy(alpha = 0.7f)) {
+                        provider.description()
+                    }
+                }
+            }
+            IconButton(onClick = onAdd) {
+                Icon(HugeIcons.Add01, contentDescription = stringResource(R.string.setting_provider_page_add))
             }
         }
     }
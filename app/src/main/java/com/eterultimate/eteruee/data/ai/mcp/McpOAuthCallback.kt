rikkahub/rikkahub

feat(mcp): 支持 MCP Server 的 OAuth 2.1 授权
- 按 MCP 授权规范(2025-11-25)实现完整 OAuth 流程:PRM/AS 元数据发现、
  动态客户端注册、PKCE、浏览器授权、授权码换取令牌与自动刷新
- 通过 Custom Tabs 打开授权页,deep link(rikkahub://mcp-oauth-callback)
  经 AppEventBus 回传,先订阅再打开浏览器避免快速回调丢失
- 连接失败时主动探测受保护资源元数据判断是否需要授权,区分手动 header 登录
- 新增授权中/需授权状态与取消授权入口
- McpOAuthState 脱敏 toString,避免 token/secret 泄露到日志

app/src/main/java/me/rerere/rikkahub/data/ai/mcp/McpOAuthCallback.kt
@@ -1,0 +1,18 @@
+package me.rerere.rikkahub.data.ai.mcp
+
+import android.content.Context
+import android.content.Intent
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.net.toUri
+
+/** OAuth 授权回调的 redirect_uri，需与 AndroidManifest 中 McpOAuthCallbackActivity 的 intent-filter 保持一致。 */
+const val MCP_OAUTH_REDIRECT_URI = "rikkahub://mcp-oauth-callback"
+
+/** 使用 Chrome Custom Tabs 打开授权 URL。 */
+fun launchOAuthAuthorization(context: Context, authorizationUrl: String) {
+    val intent = CustomTabsIntent.Builder()
+        .setShowTitle(true)
+        .build()
+    intent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    intent.launchUrl(context, authorizationUrl.toUri())
+}

app/src/main/java/me/rerere/rikkahub/ui/activity/McpOAuthCallbackActivity.kt
@@ -1,0 +1,36 @@
+package me.rerere.rikkahub.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import kotlinx.coroutines.launch
+import me.rerere.rikkahub.AppScope
+import me.rerere.rikkahub.data.event.AppEvent
+import me.rerere.rikkahub.data.event.AppEventBus
+import org.koin.android.ext.android.inject
+
+/**
+ * 透明 Activity，用于接收 MCP OAuth 授权完成后的 deep link 回调
+ * (rikkahub://mcp-oauth-callback?code=...&state=...)，解析后经 [AppEventBus] 转发。
+ */
+class McpOAuthCallbackActivity : ComponentActivity() {
+    private val eventBus by inject<AppEventBus>()
+    private val appScope by inject<AppScope>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val uri = intent?.data
+        if (uri != null) {
+            // 使用 AppScope 发送，避免 finish() 取消协程导致事件丢失
+            appScope.launch {
+                eventBus.emit(
+                    AppEvent.McpOAuthCallback(
+                        state = uri.getQueryParameter("state"),
+                        code = uri.getQueryParameter("code"),
+                        error = uri.getQueryParameter("error"),
+                    )
+                )
+            }
+        }
+        finish()
+    }
+}

EterUltimate/EterUee

app/src/main/java/com/eterultimate/eteruee/data/ai/mcp/McpOAuthCallback.kt
@@ -1,0 +1,18 @@
+package com.eterultimate.eteruee.data.ai.mcp
+
+import android.content.Context
+import android.content.Intent
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.net.toUri
+
+/** OAuth 授权回调的 redirect_uri，需与 AndroidManifest 中 McpOAuthCallbackActivity 的 intent-filter 保持一致。 */
+const val MCP_OAUTH_REDIRECT_URI = "eteruee://mcp-oauth-callback"
+
+/** 使用 Chrome Custom Tabs 打开授权 URL。 */
+fun launchOAuthAuthorization(context: Context, authorizationUrl: String) {
+    val intent = CustomTabsIntent.Builder()
+        .setShowTitle(true)
+        .build()
+    intent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    intent.launchUrl(context, authorizationUrl.toUri())
+}

app/src/main/java/com/eterultimate/eteruee/ui/activity/McpOAuthCallbackActivity.kt
@@ -1,0 +1,36 @@
+package com.eterultimate.eteruee.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import kotlinx.coroutines.launch
+import com.eterultimate.eteruee.AppScope
+import com.eterultimate.eteruee.data.event.AppEvent
+import com.eterultimate.eteruee.data.event.AppEventBus
+import org.koin.android.ext.android.inject
+
+/**
+ * 透明 Activity，用于接收 MCP OAuth 授权完成后的 deep link 回调
+ * (eteruee://mcp-oauth-callback?code=...&state=...)，解析后经 [AppEventBus] 转发。
+ */
+class McpOAuthCallbackActivity : ComponentActivity() {
+    private val eventBus by inject<AppEventBus>()
+    private val appScope by inject<AppScope>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val uri = intent?.data
+        if (uri != null) {
+            // 使用 AppScope 发送，避免 finish() 取消协程导致事件丢失
+            appScope.launch {
+                eventBus.emit(
+                    AppEvent.McpOAuthCallback(
+                        state = uri.getQueryParameter("state"),
+                        code = uri.getQueryParameter("code"),
+                        error = uri.getQueryParameter("error"),
+                    )
+                )
+            }
+        }
+        finish()
+    }
+}
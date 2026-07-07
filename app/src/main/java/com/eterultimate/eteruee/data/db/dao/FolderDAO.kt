rikkahub/rikkahub

feat(chat): 新增会话文件夹分组功能
助手内文件夹分组，会话可移入/移出文件夹，支持新建/重命名/删除；
移动助手及删除文件夹时同步内存态，避免整对象保存覆盖 folder_id；
文件夹内有生成中会话时禁止删除；文案已本地化至全部语言。

app/src/main/java/me/rerere/rikkahub/data/db/dao/FolderDAO.kt
@@ -1,0 +1,34 @@
+package me.rerere.rikkahub.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import me.rerere.rikkahub.data.db.entity.FolderEntity
+
+@Dao
+interface FolderDAO {
+    @Query("SELECT * FROM conversation_folder WHERE assistant_id = :assistantId ORDER BY sort_index ASC, create_at ASC")
+    fun getFoldersOfAssistant(assistantId: String): Flow<List<FolderEntity>>
+
+    @Query("SELECT * FROM conversation_folder WHERE id = :id")
+    suspend fun getFolderById(id: String): FolderEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(folder: FolderEntity)
+
+    @Update
+    suspend fun update(folder: FolderEntity)
+
+    @Query("UPDATE conversation_folder SET name = :name WHERE id = :id")
+    suspend fun rename(id: String, name: String)
+
+    @Delete
+    suspend fun delete(folder: FolderEntity)
+
+    @Query("DELETE FROM conversation_folder WHERE id = :id")
+    suspend fun deleteById(id: String)
+}

app/src/main/java/me/rerere/rikkahub/data/db/entity/FolderEntity.kt
@@ -1,0 +1,30 @@
+package me.rerere.rikkahub.data.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+/**
+ * 会话文件夹（助手内分组）。
+ *
+ * 一个文件夹隶属于某个助手（assistant_id），会话通过 [ConversationEntity.folderId] 关联到文件夹。
+ * 这里不使用 Room 外键，删除文件夹时由 Repository 层手动把归属会话的 folder_id 清空，
+ * 避免级联删除误删会话，也避免外键迁移的复杂度。
+ */
+@Entity(
+    tableName = "conversation_folder",
+    indices = [Index(value = ["assistant_id"])]
+)
+data class FolderEntity(
+    @PrimaryKey
+    val id: String,
+    @ColumnInfo("assistant_id")
+    val assistantId: String,
+    @ColumnInfo("name")
+    val name: String,
+    @ColumnInfo("sort_index", defaultValue = "0")
+    val sortIndex: Int = 0,
+    @ColumnInfo("create_at")
+    val createAt: Long,
+)

app/src/main/java/me/rerere/rikkahub/data/model/Folder.kt
@@ -1,0 +1,15 @@
+package me.rerere.rikkahub.data.model
+
+import java.time.Instant
+import kotlin.uuid.Uuid
+
+/**
+ * 会话文件夹（助手内分组）。
+ */
+data class Folder(
+    val id: Uuid = Uuid.random(),
+    val assistantId: Uuid,
+    val name: String,
+    val sortIndex: Int = 0,
+    val createAt: Instant = Instant.now(),
+)

app/src/main/java/me/rerere/rikkahub/di/AppModule.kt
@@ -80,7 +80,8 @@ val appModule = module {
             mcpManager = get(),
             filesManager = get(),
             skillManager = get(),
-            workspaceRepository = get()
+            workspaceRepository = get(),
+            folderRepository = get()
         )
     }
 

app/src/main/java/me/rerere/rikkahub/service/ChatService.kt
@@ -84,6 +84,7 @@ import me.rerere.rikkahub.data.model.AssistantAffectScope
 import me.rerere.rikkahub.data.model.replaceRegexes
 import me.rerere.rikkahub.data.model.toMessageNode
 import me.rerere.rikkahub.data.repository.ConversationRepository
+import me.rerere.rikkahub.data.repository.FolderRepository
 import me.rerere.rikkahub.data.repository.MemoryRepository
 import me.rerere.rikkahub.data.repository.WorkspaceRepository
 import me.rerere.rikkahub.web.BadRequestException
@@ -154,6 +155,7 @@ class ChatService(
     private val filesManager: FilesManager,
     private val skillManager: SkillManager,
     private val workspaceRepository: WorkspaceRepository,
+    private val folderRepository: FolderRepository,
 ) {
     // workspace 系统提示注入 (依赖 workspaceRepository, 故在类内构造)
     private val workspaceReminderTransformer = WorkspaceReminderTransformer(workspaceRepository)
@@ -1057,6 +1059,43 @@ class ChatService(
     fun updateConversationState(conversationId: Uuid, update: (Conversation) -> Conversation) {
         val current = getConversationFlow(conversationId).value
         updateConversation(conversationId, update(current))
+    }
+
+    /**
+     * 移动会话到文件夹（folderId 为 null 表示移出到未归类）。
+     *
+     * 若该会话当前有活跃 session（正在查看或后台生成），先同步内存态再落库：
+     * 否则仅改数据库 folder_id，而内存里那份 Conversation 仍是旧 folderId，
+     * 后续任意 saveConversation(id, state.value) 会用整对象把 folder_id 覆盖回旧值，导致移动丢失。
+     * 先改内存可确保这段窗口内的整对象保存也带上新 folderId。
+     */
+    suspend fun moveConversationToFolder(conversationId: Uuid, folderId: Uuid?) {
+        if (sessions.containsKey(conversationId)) {
+            updateConversationState(conversationId) { it.copy(folderId = folderId) }
+        }
+        conversationRepo.updateConversationFolderId(conversationId, folderId)
+    }
+
+    /**
+     * 文件夹内是否存在正在生成回复的会话。
+     * 仅活跃 session 可能在生成；内存态 folderId 为权威（移动会先同步内存态）。
+     */
+    fun hasGeneratingConversationInFolder(folderId: Uuid): Boolean {
+        return sessions.values.any { it.isGenerating && it.state.value.folderId == folderId }
+    }
+
+    /**
+     * 删除文件夹（folder_id 归属会被清空，会话本身保留）。
+     *
+     * 先把内存中归属该文件夹的活跃 session folderId 置空，再删库：
+     * 否则 clearFolder 只改了数据库，而活跃 session 内存态仍指向该文件夹，
+     * 后续整对象保存会写回一个已被删除的 folder_id，导致会话在列表中悬空。
+     */
+    suspend fun deleteFolder(folderId: Uuid) {
+        sessions.values
+            .filter { it.state.value.folderId == folderId }
+            .forEach { updateConversationState(it.id) { c -> c.copy(folderId = null) } }
+        folderRepository.deleteFolder(folderId)
     }
 
     private fun checkFilesDelete(newConversation: Conversation, oldConversation: Conversation) {

EterUltimate/EterUee

app/src/main/java/com/eterultimate/eteruee/data/db/dao/FolderDAO.kt
@@ -1,0 +1,34 @@
+package com.eterultimate.eteruee.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import com.eterultimate.eteruee.data.db.entity.FolderEntity
+
+@Dao
+interface FolderDAO {
+    @Query("SELECT * FROM conversation_folder WHERE assistant_id = :assistantId ORDER BY sort_index ASC, create_at ASC")
+    fun getFoldersOfAssistant(assistantId: String): Flow<List<FolderEntity>>
+
+    @Query("SELECT * FROM conversation_folder WHERE id = :id")
+    suspend fun getFolderById(id: String): FolderEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(folder: FolderEntity)
+
+    @Update
+    suspend fun update(folder: FolderEntity)
+
+    @Query("UPDATE conversation_folder SET name = :name WHERE id = :id")
+    suspend fun rename(id: String, name: String)
+
+    @Delete
+    suspend fun delete(folder: FolderEntity)
+
+    @Query("DELETE FROM conversation_folder WHERE id = :id")
+    suspend fun deleteById(id: String)
+}

app/src/main/java/com/eterultimate/eteruee/data/db/entity/FolderEntity.kt
@@ -1,0 +1,30 @@
+package com.eterultimate.eteruee.data.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+/**
+ * 会话文件夹（助手内分组）。
+ *
+ * 一个文件夹隶属于某个助手（assistant_id），会话通过 [ConversationEntity.folderId] 关联到文件夹。
+ * 这里不使用 Room 外键，删除文件夹时由 Repository 层手动把归属会话的 folder_id 清空，
+ * 避免级联删除误删会话，也避免外键迁移的复杂度。
+ */
+@Entity(
+    tableName = "conversation_folder",
+    indices = [Index(value = ["assistant_id"])]
+)
+data class FolderEntity(
+    @PrimaryKey
+    val id: String,
+    @ColumnInfo("assistant_id")
+    val assistantId: String,
+    @ColumnInfo("name")
+    val name: String,
+    @ColumnInfo("sort_index", defaultValue = "0")
+    val sortIndex: Int = 0,
+    @ColumnInfo("create_at")
+    val createAt: Long,
+)

app/src/main/java/com/eterultimate/eteruee/data/model/Folder.kt
@@ -1,0 +1,15 @@
+package com.eterultimate.eteruee.data.model
+
+import java.time.Instant
+import kotlin.uuid.Uuid
+
+/**
+ * 会话文件夹（助手内分组）。
+ */
+data class Folder(
+    val id: Uuid = Uuid.random(),
+    val assistantId: Uuid,
+    val name: String,
+    val sortIndex: Int = 0,
+    val createAt: Instant = Instant.now(),
+)

app/src/main/java/com/eterultimate/eteruee/di/AppModule.kt
@@ -111,7 +111,8 @@ val appModule = module {
             localTools = get(),
             mcpManager = get(),
             filesManager = get(),
-            skillManager = get()
+            skillManager = get(),
+            folderRepository = get()
         )
     }
 

app/src/main/java/com/eterultimate/eteruee/service/ChatService.kt
@@ -82,6 +82,7 @@ import com.eterultimate.eteruee.data.model.AssistantAffectScope
 import com.eterultimate.eteruee.data.model.replaceRegexes
 import com.eterultimate.eteruee.data.model.toMessageNode
 import com.eterultimate.eteruee.data.repository.ConversationRepository
+import com.eterultimate.eteruee.data.repository.FolderRepository
 import com.eterultimate.eteruee.data.repository.MemoryRepository
 import com.eterultimate.eteruee.web.BadRequestException
 import com.eterultimate.eteruee.web.NotFoundException
@@ -149,6 +150,7 @@ class ChatService(
     val mcpManager: McpManager,
     private val filesManager: FilesManager,
     private val skillManager: SkillManager,
+    private val folderRepository: FolderRepository,
 ) {
     // 统一会话管理
     private val sessions = ConcurrentHashMap<Uuid, ConversationSession>()
@@ -1040,6 +1042,43 @@ class ChatService(
     fun updateConversationState(conversationId: Uuid, update: (Conversation) -> Conversation) {
         val current = getConversationFlow(conversationId).value
         updateConversation(conversationId, update(current))
+    }
+
+    /**
+     * 移动会话到文件夹（folderId 为 null 表示移出到未归类）。
+     *
+     * 若该会话当前有活跃 session（正在查看或后台生成），先同步内存态再落库：
+     * 否则仅改数据库 folder_id，而内存里那份 Conversation 仍是旧 folderId，
+     * 后续任意 saveConversation(id, state.value) 会用整对象把 folder_id 覆盖回旧值，导致移动丢失。
+     * 先改内存可确保这段窗口内的整对象保存也带上新 folderId。
+     */
+    suspend fun moveConversationToFolder(conversationId: Uuid, folderId: Uuid?) {
+        if (sessions.containsKey(conversationId)) {
+            updateConversationState(conversationId) { it.copy(folderId = folderId) }
+        }
+        conversationRepo.updateConversationFolderId(conversationId, folderId)
+    }
+
+    /**
+     * 文件夹内是否存在正在生成回复的会话。
+     * 仅活跃 session 可能在生成；内存态 folderId 为权威（移动会先同步内存态）。
+     */
+    fun hasGeneratingConversationInFolder(folderId: Uuid): Boolean {
+        return sessions.values.any { it.isGenerating && it.state.value.folderId == folderId }
+    }
+
+    /**
+     * 删除文件夹（folder_id 归属会被清空，会话本身保留）。
+     *
+     * 先把内存中归属该文件夹的活跃 session folderId 置空，再删库：
+     * 否则 clearFolder 只改了数据库，而活跃 session 内存态仍指向该文件夹，
+     * 后续整对象保存会写回一个已被删除的 folder_id，导致会话在列表中悬空。
+     */
+    suspend fun deleteFolder(folderId: Uuid) {
+        sessions.values
+            .filter { it.state.value.folderId == folderId }
+            .forEach { updateConversationState(it.id) { c -> c.copy(folderId = null) } }
+        folderRepository.deleteFolder(folderId)
     }
 
     private fun checkFilesDelete(newConversation: Conversation, oldConversation: Conversation) {
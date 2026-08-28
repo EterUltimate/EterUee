rikkahub/rikkahub

refactor(mcp): 拆分 McpManager 并暴露完整错误详情
将臃肿的 McpManager 按职责拆分为门面 + 会话注册表(McpSessionRegistry)
+ OAuth 协调器(McpOAuthCoordinator)，引入 connectionKey 机制使工具元数据
变更不再触发重连，并补充连接键单元测试。

同时保留完整错误信息：McpStatus.Error 新增 detail 字段(含 cause 链与堆栈)，
UI 支持点击展开错误详情并复制。

app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingMcpPage.kt
@@ -1,6 +1,7 @@
 package me.rerere.rikkahub.ui.pages.setting
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,8 +25,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -102,6 +106,7 @@ import me.rerere.rikkahub.ui.hooks.EditStateContent
 import me.rerere.rikkahub.ui.hooks.useEditState
 import me.rerere.rikkahub.ui.theme.CustomColors
 import me.rerere.rikkahub.ui.theme.extendColors
+import me.rerere.rikkahub.utils.writeClipboardText
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
@@ -248,6 +253,42 @@ private fun McpServerItem(
     val status by mcpManager.getStatus(item).collectAsStateWithLifecycle(McpStatus.Idle)
     val dismissBoxState = rememberSwipeToDismissBoxState()
     val scope = rememberCoroutineScope()
+    var errorDetail by remember { mutableStateOf<McpStatus.Error?>(null) }
+
+    errorDetail?.let { error ->
+        val context = LocalContext.current
+        val fullText = error.detail ?: error.message
+        AlertDialog(
+            onDismissRequest = { errorDetail = null },
+            title = { Text(item.commonOptions.name.ifBlank { "MCP" }) },
+            text = {
+                SelectionContainer {
+                    Text(
+                        text = fullText,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier
+                            .heightIn(max = 320.dp)
+                            .verticalScroll(rememberScrollState()),
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        context.writeClipboardText(fullText)
+                        errorDetail = null
+                    }
+                ) {
+                    Text(stringResource(R.string.copy))
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { errorDetail = null }) {
+                    Text(stringResource(R.string.cancel))
+                }
+            },
+        )
+    }
     SwipeToDismissBox(
         state = dismissBoxState,
         backgroundContent = {
@@ -343,12 +384,14 @@ private fun McpServerItem(
                         }
                     }
                     if (status is McpStatus.Error) {
+                        val error = status as McpStatus.Error
                         Text(
-                            text = (status as McpStatus.Error).message,
+                            text = error.message,
                             style = MaterialTheme.typography.labelSmall,
                             color = MaterialTheme.colorScheme.error,
                             maxLines = 3,
                             overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.clickable { errorDetail = error },
                         )
                     }
                     if (status == McpStatus.NeedsAuthorization) {

app/src/test/java/me/rerere/rikkahub/data/ai/mcp/McpConnectionKeyTest.kt
@@ -1,0 +1,60 @@
+package me.rerere.rikkahub.data.ai.mcp
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class McpConnectionKeyTest {
+    private val base = McpServerConfig.StreamableHTTPServer(
+        commonOptions = McpCommonOptions(name = "demo"),
+        url = "https://example.com/mcp",
+    )
+
+    @Test
+    fun `tool metadata does not affect connection key`() {
+        val withTools = base.copy(
+            commonOptions = base.commonOptions.copy(
+                tools = listOf(McpTool(name = "search", enable = false))
+            )
+        )
+
+        assertEquals(base.connectionKey(), withTools.connectionKey())
+    }
+
+    @Test
+    fun `url transport and headers affect connection key`() {
+        assertNotEquals(base.connectionKey(), base.copy(url = "https://example.com/other").connectionKey())
+        assertNotEquals(
+            base.connectionKey(),
+            McpServerConfig.SseTransportServer(
+                id = base.id,
+                commonOptions = base.commonOptions,
+                url = base.url,
+            ).connectionKey()
+        )
+        assertNotEquals(
+            base.connectionKey(),
+            base.copy(
+                commonOptions = base.commonOptions.copy(headers = listOf("X-API-Key" to "secret"))
+            ).connectionKey()
+        )
+    }
+
+    @Test
+    fun `oauth token affects connection key unless manual authorization header wins`() {
+        val oauth = McpOAuthState(enabled = true, accessToken = "oauth-token")
+        val withOAuth = base.copy(commonOptions = base.commonOptions.copy(oauth = oauth))
+        assertNotEquals(base.connectionKey(), withOAuth.connectionKey())
+
+        val manualAuth = base.copy(
+            commonOptions = base.commonOptions.copy(
+                headers = listOf("Authorization" to "Bearer manual"),
+                oauth = oauth,
+            )
+        )
+        val manualAuthWithoutOAuth = manualAuth.copy(
+            commonOptions = manualAuth.commonOptions.copy(oauth = null)
+        )
+        assertEquals(manualAuthWithoutOAuth.connectionKey(), manualAuth.connectionKey())
+    }
+}

EterUltimate/EterUee

(cherry picked from commit 0f5b3f3e56fe3a00620eb33dfa83700016ef17e3)

app/src/main/java/com/eterultimate/eteruee/ui/pages/setting/SettingMcpPage.kt
@@ -14,6 +14,7 @@ import me.rerere.hugeicons.stroke.Upload02
 import me.rerere.hugeicons.stroke.Cancel01
 import me.rerere.hugeicons.stroke.Search01
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,6 +29,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -37,8 +39,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -115,6 +119,7 @@ import com.eterultimate.eteruee.ui.hooks.EditStateContent
 import com.eterultimate.eteruee.ui.hooks.useEditState
 import com.eterultimate.eteruee.ui.theme.CustomColors
 import com.eterultimate.eteruee.ui.theme.extendColors
+import com.eterultimate.eteruee.utils.writeClipboardText
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 
@@ -268,6 +273,42 @@ private fun McpServerItem(
     val status by mcpManager.getStatus(item).collectAsStateWithLifecycle(McpStatus.Idle)
     val dismissBoxState = rememberSwipeToDismissBoxState()
     val scope = rememberCoroutineScope()
+    var errorDetail by remember { mutableStateOf<McpStatus.Error?>(null) }
+
+    errorDetail?.let { error ->
+        val context = LocalContext.current
+        val fullText = error.detail ?: error.message
+        AlertDialog(
+            onDismissRequest = { errorDetail = null },
+            title = { Text(item.commonOptions.name.ifBlank { "MCP" }) },
+            text = {
+                SelectionContainer {
+                    Text(
+                        text = fullText,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier
+                            .heightIn(max = 320.dp)
+                            .verticalScroll(rememberScrollState()),
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        context.writeClipboardText(fullText)
+                        errorDetail = null
+                    }
+                ) {
+                    Text(stringResource(R.string.copy))
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { errorDetail = null }) {
+                    Text(stringResource(R.string.cancel))
+                }
+            },
+        )
+    }
     SwipeToDismissBox(
         state = dismissBoxState,
         backgroundContent = {
@@ -364,12 +405,14 @@ private fun McpServerItem(
                         }
                     }
                     if (status is McpStatus.Error) {
+                        val error = status as McpStatus.Error
                         Text(
-                            text = (status as McpStatus.Error).message,
+                            text = error.message,
                             style = MaterialTheme.typography.labelSmall,
                             color = MaterialTheme.colorScheme.error,
                             maxLines = 3,
                             overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.clickable { errorDetail = error },
                         )
                     }
                     if (status == McpStatus.NeedsAuthorization) {

app/src/test/java/com/eterultimate/eteruee/data/ai/mcp/McpConnectionKeyTest.kt
@@ -1,0 +1,60 @@
+package com.eterultimate.eteruee.data.ai.mcp
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class McpConnectionKeyTest {
+    private val base = McpServerConfig.StreamableHTTPServer(
+        commonOptions = McpCommonOptions(name = "demo"),
+        url = "https://example.com/mcp",
+    )
+
+    @Test
+    fun `tool metadata does not affect connection key`() {
+        val withTools = base.copy(
+            commonOptions = base.commonOptions.copy(
+                tools = listOf(McpTool(name = "search", enable = false))
+            )
+        )
+
+        assertEquals(base.connectionKey(), withTools.connectionKey())
+    }
+
+    @Test
+    fun `url transport and headers affect connection key`() {
+        assertNotEquals(base.connectionKey(), base.copy(url = "https://example.com/other").connectionKey())
+        assertNotEquals(
+            base.connectionKey(),
+            McpServerConfig.SseTransportServer(
+                id = base.id,
+                commonOptions = base.commonOptions,
+                url = base.url,
+            ).connectionKey()
+        )
+        assertNotEquals(
+            base.connectionKey(),
+            base.copy(
+                commonOptions = base.commonOptions.copy(headers = listOf("X-API-Key" to "secret"))
+            ).connectionKey()
+        )
+    }
+
+    @Test
+    fun `oauth token affects connection key unless manual authorization header wins`() {
+        val oauth = McpOAuthState(enabled = true, accessToken = "oauth-token")
+        val withOAuth = base.copy(commonOptions = base.commonOptions.copy(oauth = oauth))
+        assertNotEquals(base.connectionKey(), withOAuth.connectionKey())
+
+        val manualAuth = base.copy(
+            commonOptions = base.commonOptions.copy(
+                headers = listOf("Authorization" to "Bearer manual"),
+                oauth = oauth,
+            )
+        )
+        val manualAuthWithoutOAuth = manualAuth.copy(
+            commonOptions = manualAuth.commonOptions.copy(oauth = null)
+        )
+        assertEquals(manualAuthWithoutOAuth.connectionKey(), manualAuth.connectionKey())
+    }
+}
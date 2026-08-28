rikkahub/rikkahub

feat(search): 网络搜索开关改为每个助手独立存储
将 enableWebSearch 从全局 Settings 迁移到 Assistant，使每个助手
独立记忆是否启用网络搜索（与模型选择一致）；搜索服务选择仍保持全局。
同步更新 Web 端接口与前端读写。

app/src/main/java/me/rerere/rikkahub/ui/pages/chat/ChatVM.kt
@@ -26,6 +26,7 @@ import me.rerere.ai.ui.isEmptyInputMessage
 import me.rerere.rikkahub.R
 import me.rerere.rikkahub.data.datastore.Settings
 import me.rerere.rikkahub.data.datastore.SettingsStore
+import me.rerere.rikkahub.data.datastore.getCurrentAssistant
 import me.rerere.rikkahub.data.datastore.getCurrentChatModel
 import me.rerere.rikkahub.data.files.FilesManager
 import me.rerere.rikkahub.data.model.Assistant
@@ -101,9 +102,9 @@ class ChatVM(
     val settings: StateFlow<Settings> =
         settingsStore.settingsFlow.stateIn(viewModelScope, SharingStarted.Eagerly, Settings.dummy())
 
-    // 网络搜索
+    // 网络搜索(每个助手独立)
     val enableWebSearch = settings.map {
-        it.enableWebSearch
+        it.getCurrentAssistant().enableWebSearch
     }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     // 当前模型

EterUltimate/EterUee

(cherry picked from commit 0183033295cbf5e4700472c4eab7d2a9524efbfa)

app/src/main/java/com/eterultimate/eteruee/ui/pages/chat/ChatVM.kt
@@ -26,6 +26,7 @@ import com.eterultimate.eteruee.ai.ui.isEmptyInputMessage
 import com.eterultimate.eteruee.R
 import com.eterultimate.eteruee.data.datastore.Settings
 import com.eterultimate.eteruee.data.datastore.SettingsStore
+import com.eterultimate.eteruee.data.datastore.getCurrentAssistant
 import com.eterultimate.eteruee.data.datastore.getCurrentChatModel
 import com.eterultimate.eteruee.data.files.FilesManager
 import com.eterultimate.eteruee.data.model.Assistant
@@ -117,9 +118,13 @@ class ChatVM(
         chatService.removeConversationReference(_conversationId)
     }
 
-    // 网络搜索
+    // 用户设置
+    val settings: StateFlow<Settings> =
+        settingsStore.settingsFlow.stateIn(viewModelScope, SharingStarted.Eagerly, Settings.dummy())
+
+    // 网络搜索(每个助手独立)
     val enableWebSearch = settings.map {
-        it.enableWebSearch
+        it.getCurrentAssistant().enableWebSearch
     }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     // 当前模型
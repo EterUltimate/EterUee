rikkahub/rikkahub

feat: TTS不朗读括号内的内容，与引号过滤叠加生效 (#1473)

app/src/main/java/me/rerere/rikkahub/ui/components/message/ChatMessageActions.kt
@@ -61,6 +61,7 @@ import me.rerere.rikkahub.ui.context.LocalSettings
 import me.rerere.rikkahub.ui.context.LocalTTSState
 import me.rerere.rikkahub.utils.copyMessageToClipboard
 import me.rerere.rikkahub.utils.extractQuotedContentAsText
+import me.rerere.rikkahub.utils.removeBracketedContent
 import me.rerere.rikkahub.utils.toLocalString
 import me.rerere.rikkahub.utils.toMessageTimeString
 import java.util.Locale
@@ -138,10 +139,12 @@ fun ColumnScope.ChatMessageActionButtons(
                         onClick = {
                             if (!isSpeaking) {
                                 val text = message.toText()
-                                val textToSpeak = if (settings.displaySetting.ttsOnlyReadQuoted) {
-                                    text.extractQuotedContentAsText() ?: text
-                                } else {
-                                    text
+                                var textToSpeak = text
+                                if (settings.displaySetting.ttsOnlyReadQuoted) {
+                                    textToSpeak = textToSpeak.extractQuotedContentAsText() ?: textToSpeak
+                                }
+                                if (settings.displaySetting.ttsOnlyReadOutsideBrackets) {
+                                    textToSpeak = textToSpeak.removeBracketedContent() ?: textToSpeak
                                 }
                                 tts.speak(textToSpeak)
                             } else {

app/src/main/java/me/rerere/rikkahub/ui/pages/chat/TTSAutoPlay.kt
@@ -9,6 +9,7 @@ import me.rerere.rikkahub.data.datastore.Settings
 import me.rerere.rikkahub.data.model.Conversation
 import me.rerere.rikkahub.ui.context.LocalTTSState
 import me.rerere.rikkahub.utils.extractQuotedContentAsText
+import me.rerere.rikkahub.utils.removeBracketedContent
 
 @Composable
 fun TTSAutoPlay(vm: ChatVM, setting: Settings, conversation: Conversation) {
@@ -22,10 +23,12 @@ fun TTSAutoPlay(vm: ChatVM, setting: Settings, conversation: Conversation) {
                 val lastMessage = currentConversation.currentMessages.lastOrNull()
                 if (lastMessage != null && lastMessage.role == MessageRole.ASSISTANT) {
                     val text = lastMessage.toText()
-                    val textToSpeak = if (updatedSetting.displaySetting.ttsOnlyReadQuoted) {
-                        text.extractQuotedContentAsText() ?: text
-                    } else {
-                        text
+                    var textToSpeak = text
+                    if (updatedSetting.displaySetting.ttsOnlyReadQuoted) {
+                        textToSpeak = textToSpeak.extractQuotedContentAsText() ?: textToSpeak
+                    }
+                    if (updatedSetting.displaySetting.ttsOnlyReadOutsideBrackets) {
+                        textToSpeak = textToSpeak.removeBracketedContent() ?: textToSpeak
                     }
                     if (textToSpeak.isNotBlank()) {
                         tts.speak(textToSpeak)

EterUltimate/EterUee

(cherry picked from commit b6b168428198b60d869e6ac3e8621c801c693fbf)

app/src/main/java/com/eterultimate/eteruee/ui/components/message/ChatMessageActions.kt
@@ -61,6 +61,7 @@ import com.eterultimate.eteruee.ui.context.LocalSettings
 import com.eterultimate.eteruee.ui.context.LocalTTSState
 import com.eterultimate.eteruee.utils.copyMessageToClipboard
 import com.eterultimate.eteruee.utils.extractQuotedContentAsText
+import com.eterultimate.eteruee.utils.removeBracketedContent
 import com.eterultimate.eteruee.utils.toLocalString
 import java.util.Locale
 import androidx.compose.ui.graphics.RectangleShape
@@ -134,10 +135,12 @@ fun ColumnScope.ChatMessageActionButtons(
                         onClick = {
                             if (!isSpeaking) {
                                 val text = message.toText()
-                                val textToSpeak = if (settings.displaySetting.ttsOnlyReadQuoted) {
-                                    text.extractQuotedContentAsText() ?: text
-                                } else {
-                                    text
+                                var textToSpeak = text
+                                if (settings.displaySetting.ttsOnlyReadQuoted) {
+                                    textToSpeak = textToSpeak.extractQuotedContentAsText() ?: textToSpeak
+                                }
+                                if (settings.displaySetting.ttsOnlyReadOutsideBrackets) {
+                                    textToSpeak = textToSpeak.removeBracketedContent() ?: textToSpeak
                                 }
                                 tts.speak(textToSpeak)
                             } else {

app/src/main/java/com/eterultimate/eteruee/ui/pages/chat/TTSAutoPlay.kt
@@ -9,6 +9,7 @@ import com.eterultimate.eteruee.data.datastore.Settings
 import com.eterultimate.eteruee.data.model.Conversation
 import com.eterultimate.eteruee.ui.context.LocalTTSState
 import com.eterultimate.eteruee.utils.extractQuotedContentAsText
+import com.eterultimate.eteruee.utils.removeBracketedContent
 
 @Composable
 fun TTSAutoPlay(vm: ChatVM, setting: Settings, conversation: Conversation) {
@@ -22,10 +23,12 @@ fun TTSAutoPlay(vm: ChatVM, setting: Settings, conversation: Conversation) {
                 val lastMessage = currentConversation.currentMessages.lastOrNull()
                 if (lastMessage != null && lastMessage.role == MessageRole.ASSISTANT) {
                     val text = lastMessage.toText()
-                    val textToSpeak = if (updatedSetting.displaySetting.ttsOnlyReadQuoted) {
-                        text.extractQuotedContentAsText() ?: text
-                    } else {
-                        text
+                    var textToSpeak = text
+                    if (updatedSetting.displaySetting.ttsOnlyReadQuoted) {
+                        textToSpeak = textToSpeak.extractQuotedContentAsText() ?: textToSpeak
+                    }
+                    if (updatedSetting.displaySetting.ttsOnlyReadOutsideBrackets) {
+                        textToSpeak = textToSpeak.removeBracketedContent() ?: textToSpeak
                     }
                     if (textToSpeak.isNotBlank()) {
                         tts.speak(textToSpeak)
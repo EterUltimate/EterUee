rikkahub/rikkahub

fix(ui): 图片裁剪失败时提示错误而非静默忽略
UCrop 返回 RESULT_ERROR 时（如动态 WebP 解码失败）此前被静默吞掉，
用户表现为"上传头像没反应"。现在弹出错误 toast 并记录日志。

app/src/main/java/me/rerere/rikkahub/ui/components/ai/CropLauncher.kt
@@ -13,9 +13,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toFile
+import com.dokar.sonner.ToastType
 import com.yalantis.ucrop.UCrop
 import com.yalantis.ucrop.UCropActivity
+import me.rerere.common.android.Logging
 import me.rerere.common.android.appTempFolder
+import me.rerere.rikkahub.ui.context.LocalToaster
 import java.io.File
 
 @Composable
@@ -26,14 +29,29 @@ internal fun useCropLauncher(
     freeStyleCropEnabled: Boolean = true
 ): Pair<ActivityResultLauncher<Intent>, (Uri) -> Unit> {
     val context = LocalContext.current
+    val toaster = LocalToaster.current
     var cropOutputUri by remember { mutableStateOf<Uri?>(null) }
 
     val cropActivityLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
-        if (result.resultCode == android.app.Activity.RESULT_OK) {
-            cropOutputUri?.let { croppedUri ->
-                onCroppedImageReady(croppedUri)
+        when (result.resultCode) {
+            android.app.Activity.RESULT_OK -> {
+                cropOutputUri?.let { croppedUri ->
+                    onCroppedImageReady(croppedUri)
+                }
+            }
+
+            UCrop.RESULT_ERROR -> {
+                val error = result.data?.let { UCrop.getError(it) }
+                Logging.log(
+                    "CropLauncher",
+                    "crop failed: ${error?.message} | ${error?.stackTraceToString()}"
+                )
+                toaster.show(
+                    "Failed to crop image: ${error?.message ?: "unknown error"}",
+                    type = ToastType.Error
+                )
             }
         }
         cropOutputUri?.toFile()?.delete()

EterUltimate/EterUee

(cherry picked from commit fe3037c5b263585f49e7df33bcc100ec9da9e942)

app/src/main/java/com/eterultimate/eteruee/ui/components/ai/CropLauncher.kt
@@ -16,6 +16,12 @@ import androidx.core.net.toFile
 import com.eterultimate.eteruee.utils.appTempFolder
 import com.yalantis.ucrop.UCrop
 import com.yalantis.ucrop.UCropActivity
+import com.dokar.sonner.ToastType
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.UCropActivity
+import com.eterultimate.eteruee.common.android.Logging
+
+import com.eterultimate.eteruee.ui.context.LocalToaster
 import java.io.File
 
 @Composable
@@ -26,14 +32,29 @@ internal fun useCropLauncher(
     freeStyleCropEnabled: Boolean = true
 ): Pair<ActivityResultLauncher<Intent>, (Uri) -> Unit> {
     val context = LocalContext.current
+    val toaster = LocalToaster.current
     var cropOutputUri by remember { mutableStateOf<Uri?>(null) }
 
     val cropActivityLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
-        if (result.resultCode == android.app.Activity.RESULT_OK) {
-            cropOutputUri?.let { croppedUri ->
-                onCroppedImageReady(croppedUri)
+        when (result.resultCode) {
+            android.app.Activity.RESULT_OK -> {
+                cropOutputUri?.let { croppedUri ->
+                    onCroppedImageReady(croppedUri)
+                }
+            }
+
+            UCrop.RESULT_ERROR -> {
+                val error = result.data?.let { UCrop.getError(it) }
+                Logging.log(
+                    "CropLauncher",
+                    "crop failed: ${error?.message} | ${error?.stackTraceToString()}"
+                )
+                toaster.show(
+                    "Failed to crop image: ${error?.message ?: "unknown error"}",
+                    type = ToastType.Error
+                )
             }
         }
         cropOutputUri?.toFile()?.delete()
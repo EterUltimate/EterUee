rikkahub/rikkahub

fix(webview): 预览内容改为缓存到文件避免导航状态过大崩溃
将 WebView 预览的 HTML 内容从导航参数中移出，改为写入 cacheDir 并仅
传递 SHA-256 内容 ID，避免大段 HTML 撑爆 SaveableStateRegistry 触发
TransactionTooLargeException。缓存按 SHA-256 去重，7 天未访问自动清理。

Fixes #1485

app/src/main/java/me/rerere/rikkahub/ui/components/message/ChatMessage.kt
@@ -82,6 +82,7 @@ import me.rerere.rikkahub.data.model.replaceRegexes
 import me.rerere.rikkahub.ui.components.richtext.MarkdownBlock
 import me.rerere.rikkahub.ui.components.richtext.ZoomableAsyncImage
 import me.rerere.rikkahub.ui.components.richtext.buildMarkdownPreviewHtml
+import me.rerere.rikkahub.ui.components.webview.WebViewContentCache
 import me.rerere.rikkahub.ui.components.ui.ChainOfThought
 import me.rerere.rikkahub.ui.components.ui.Favicon
 import me.rerere.rikkahub.ui.context.LocalNavController
@@ -91,7 +92,6 @@ import me.rerere.rikkahub.ui.theme.LocalChatFontFamily
 import me.rerere.rikkahub.ui.theme.rememberChatFontFamily
 import me.rerere.rikkahub.ui.theme.extendColors
 import me.rerere.rikkahub.utils.JsonInstant
-import me.rerere.rikkahub.utils.base64Encode
 import me.rerere.rikkahub.utils.openUrl
 import me.rerere.rikkahub.utils.urlDecode
 import java.util.Locale
@@ -241,7 +241,8 @@ fun ChatMessage(
                         markdown = textContent,
                         colorScheme = colorScheme
                     )
-                    navController.navigate(Screen.WebView(content = htmlContent.base64Encode()))
+                    val contentId = WebViewContentCache.store(context.cacheDir, htmlContent)
+                    navController.navigate(Screen.WebView(contentId = contentId))
                 }
             },
             onDismissRequest = {

app/src/main/java/me/rerere/rikkahub/ui/components/richtext/HighlightCodeBlock.kt
@@ -71,6 +71,7 @@ import me.rerere.hugeicons.stroke.View
 import me.rerere.rikkahub.R
 import me.rerere.rikkahub.Screen
 import me.rerere.rikkahub.ui.components.webview.WebView
+import me.rerere.rikkahub.ui.components.webview.WebViewContentCache
 import me.rerere.rikkahub.ui.components.webview.rememberWebViewState
 import me.rerere.rikkahub.ui.context.LocalNavController
 import me.rerere.rikkahub.ui.context.LocalSettings
@@ -80,7 +81,6 @@ import me.rerere.rikkahub.ui.theme.AtomOneDarkPalette
 import me.rerere.rikkahub.ui.theme.AtomOneLightPalette
 import me.rerere.rikkahub.ui.theme.JetbrainsMono
 import me.rerere.rikkahub.ui.theme.LocalDarkMode
-import me.rerere.rikkahub.utils.base64Encode
 import me.rerere.rikkahub.utils.toDp
 import kotlin.time.Clock
 
@@ -365,6 +365,7 @@ private fun HighlightCodeActions(
     canInlinePreview: Boolean = false,
     onTogglePreviewMode: () -> Unit = {},
 ) {
+    val context = LocalContext.current
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
@@ -461,7 +462,8 @@ private fun HighlightCodeActions(
                         .clip(RoundedCornerShape(4.dp))
                         .onClick {
                             val content = buildCodePreviewHtml(code = code, language = normalizedLanguage)
-                            navController.navigate(Screen.WebView(content = content.base64Encode()))
+                            val contentId = WebViewContentCache.store(context.cacheDir, content)
+                            navController.navigate(Screen.WebView(contentId = contentId))
                         }
                         .padding(4.dp)
                         .size(iconSize)

app/src/main/java/me/rerere/rikkahub/ui/components/webview/WebViewContentCache.kt
@@ -1,0 +1,65 @@
+package me.rerere.rikkahub.ui.components.webview
+
+import java.io.File
+import java.security.MessageDigest
+import java.util.concurrent.TimeUnit
+
+internal object WebViewContentCache {
+    private const val DIRECTORY_NAME = "webview_content"
+    private const val HASH_LENGTH = 64
+    private val maxAgeMillis = TimeUnit.DAYS.toMillis(7)
+    private val hexDigits = "0123456789abcdef".toCharArray()
+
+    fun store(cacheDir: File, content: String): String {
+        val id = content.sha256()
+        val directory = File(cacheDir, DIRECTORY_NAME)
+        check(directory.isDirectory || directory.mkdirs()) {
+            "Unable to create WebView content cache directory"
+        }
+
+        val file = File(directory, id)
+        if (!file.isFile) {
+            file.writeText(content)
+        }
+        file.setLastModified(System.currentTimeMillis())
+
+        removeExpiredFiles(directory)
+        return id
+    }
+
+    fun load(cacheDir: File, id: String): String? {
+        if (!id.isSha256()) return null
+
+        val file = File(File(cacheDir, DIRECTORY_NAME), id)
+        if (!file.isFile) return null
+
+        return runCatching {
+            file.readText().also {
+                file.setLastModified(System.currentTimeMillis())
+            }
+        }.getOrNull()
+    }
+
+    private fun removeExpiredFiles(directory: File) {
+        val expirationTime = System.currentTimeMillis() - maxAgeMillis
+        directory.listFiles()?.forEach { file ->
+            if (file.isFile && file.lastModified() < expirationTime) {
+                file.delete()
+            }
+        }
+    }
+
+    private fun String.sha256(): String {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(toByteArray())
+        return CharArray(bytes.size * 2).also { result ->
+            bytes.forEachIndexed { index, byte ->
+                val value = byte.toInt() and 0xff
+                result[index * 2] = hexDigits[value ushr 4]
+                result[index * 2 + 1] = hexDigits[value and 0x0f]
+            }
+        }.concatToString()
+    }
+
+    private fun String.isSha256(): Boolean =
+        length == HASH_LENGTH && all { it in '0'..'9' || it in 'a'..'f' }
+}

app/src/main/java/me/rerere/rikkahub/ui/pages/webview/WebViewPage.kt
@@ -31,19 +31,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import me.rerere.hugeicons.stroke.MoreVertical
 import me.rerere.rikkahub.ui.components.nav.BackButton
 import me.rerere.rikkahub.ui.components.webview.WebView
+import me.rerere.rikkahub.ui.components.webview.WebViewContentCache
 import me.rerere.rikkahub.ui.components.webview.rememberWebViewState
 import me.rerere.rikkahub.ui.theme.JetbrainsMono
-import me.rerere.rikkahub.utils.base64Decode
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WebViewPage(url: String, content: String) {
+fun WebViewPage(url: String, contentId: String) {
+    val context = LocalContext.current
     val state = if (url.isNotEmpty()) {
         rememberWebViewState(
             url = url,
@@ -54,8 +56,11 @@ fun WebViewPage(url: String, content: String) {
                 loadWithOverviewMode = true
             })
     } else {
+        val content = remember(contentId) {
+            WebViewContentCache.load(context.cacheDir, contentId).orEmpty()
+        }
         rememberWebViewState(
-            data = content.base64Decode(),
+            data = content,
             baseUrl = "https://rikkahub.local",
             mimeType = "text/html",
             settings = {

app/src/test/java/me/rerere/rikkahub/ui/components/webview/WebViewContentCacheTest.kt
@@ -1,0 +1,38 @@
+package me.rerere.rikkahub.ui.components.webview
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class WebViewContentCacheTest {
+    @get:Rule
+    val temporaryFolder = TemporaryFolder()
+
+    @Test
+    fun `stores large content outside navigation state`() {
+        val content = "<html>${"preview".repeat(50_000)}</html>"
+
+        val id = WebViewContentCache.store(temporaryFolder.root, content)
+
+        assertEquals(64, id.length)
+        assertEquals(content, WebViewContentCache.load(temporaryFolder.root, id))
+    }
+
+    @Test
+    fun `reuses the same cache entry for identical content`() {
+        val content = "<html>preview</html>"
+
+        val firstId = WebViewContentCache.store(temporaryFolder.root, content)
+        val secondId = WebViewContentCache.store(temporaryFolder.root, content)
+
+        assertEquals(firstId, secondId)
+        assertEquals(1, temporaryFolder.root.resolve("webview_content").listFiles()?.size)
+    }
+
+    @Test
+    fun `rejects invalid cache ids`() {
+        assertNull(WebViewContentCache.load(temporaryFolder.root, "../content"))
+    }
+}

EterUltimate/EterUee

(cherry picked from commit d910c4a1c306fb3fe54e4e51641b01c3106af3df)

app/src/main/java/com/eterultimate/eteruee/ui/components/message/ChatMessage.kt
@@ -88,6 +88,7 @@ import com.eterultimate.eteruee.data.model.replaceRegexes
 import com.eterultimate.eteruee.ui.components.richtext.MarkdownBlock
 import com.eterultimate.eteruee.ui.components.richtext.ZoomableAsyncImage
 import com.eterultimate.eteruee.ui.components.richtext.buildMarkdownPreviewHtml
+import com.eterultimate.eteruee.ui.components.webview.WebViewContentCache
 import com.eterultimate.eteruee.ui.components.ui.ChainOfThought
 import com.eterultimate.eteruee.ui.components.ui.Favicon
 import com.eterultimate.eteruee.ui.components.ui.RabbitLoadingIndicator
@@ -97,7 +98,6 @@ import com.eterultimate.eteruee.ui.context.LocalSettings
 import com.eterultimate.eteruee.ui.theme.extendColors
 import com.eterultimate.eteruee.data.datastore.ChatFontFamily
 import com.eterultimate.eteruee.utils.JsonInstant
-import com.eterultimate.eteruee.utils.base64Encode
 import com.eterultimate.eteruee.utils.openUrl
 import com.eterultimate.eteruee.utils.urlDecode
 import java.util.Locale
@@ -265,7 +265,8 @@ fun ChatMessage(
                         markdown = textContent,
                         colorScheme = colorScheme
                     )
-                    navController.navigate(Screen.WebView(content = htmlContent.base64Encode()))
+                    val contentId = WebViewContentCache.store(context.cacheDir, htmlContent)
+                    navController.navigate(Screen.WebView(contentId = contentId))
                 }
             },
             onDismissRequest = {

app/src/main/java/com/eterultimate/eteruee/ui/components/richtext/HighlightCodeBlock.kt
@@ -77,6 +77,9 @@ import com.eterultimate.eteruee.R
 import com.eterultimate.eteruee.Screen
 import com.eterultimate.eteruee.data.ai.tools.executeJavaScriptCode
 import com.eterultimate.eteruee.data.ai.tools.executePythonScript
+import com.eterultimate.eteruee.ui.components.webview.WebView
+import com.eterultimate.eteruee.ui.components.webview.WebViewContentCache
+import com.eterultimate.eteruee.ui.components.webview.rememberWebViewState
 import com.eterultimate.eteruee.ui.context.LocalNavController
 import com.eterultimate.eteruee.ui.context.LocalSettings
 import com.eterultimate.eteruee.ui.context.Navigator
@@ -85,7 +88,6 @@ import com.eterultimate.eteruee.ui.theme.AtomOneDarkPalette
 import com.eterultimate.eteruee.ui.theme.AtomOneLightPalette
 import com.eterultimate.eteruee.ui.theme.JetbrainsMono
 import com.eterultimate.eteruee.ui.theme.LocalDarkMode
-import com.eterultimate.eteruee.utils.base64Encode
 import com.eterultimate.eteruee.utils.toDp
 import kotlin.time.Clock
 
@@ -545,7 +547,8 @@ private fun HighlightCodeActions(
                                 }
                                 else -> code
                             }
-                            navController.navigate(Screen.WebView(content = content.base64Encode()))
+                            val contentId = WebViewContentCache.store(context.cacheDir, content)
+                            navController.navigate(Screen.WebView(contentId = contentId))
                         }
                         .padding(4.dp)
                         .size(iconSize)

app/src/main/java/com/eterultimate/eteruee/ui/components/webview/WebViewContentCache.kt
@@ -1,0 +1,65 @@
+package com.eterultimate.eteruee.ui.components.webview
+
+import java.io.File
+import java.security.MessageDigest
+import java.util.concurrent.TimeUnit
+
+internal object WebViewContentCache {
+    private const val DIRECTORY_NAME = "webview_content"
+    private const val HASH_LENGTH = 64
+    private val maxAgeMillis = TimeUnit.DAYS.toMillis(7)
+    private val hexDigits = "0123456789abcdef".toCharArray()
+
+    fun store(cacheDir: File, content: String): String {
+        val id = content.sha256()
+        val directory = File(cacheDir, DIRECTORY_NAME)
+        check(directory.isDirectory || directory.mkdirs()) {
+            "Unable to create WebView content cache directory"
+        }
+
+        val file = File(directory, id)
+        if (!file.isFile) {
+            file.writeText(content)
+        }
+        file.setLastModified(System.currentTimeMillis())
+
+        removeExpiredFiles(directory)
+        return id
+    }
+
+    fun load(cacheDir: File, id: String): String? {
+        if (!id.isSha256()) return null
+
+        val file = File(File(cacheDir, DIRECTORY_NAME), id)
+        if (!file.isFile) return null
+
+        return runCatching {
+            file.readText().also {
+                file.setLastModified(System.currentTimeMillis())
+            }
+        }.getOrNull()
+    }
+
+    private fun removeExpiredFiles(directory: File) {
+        val expirationTime = System.currentTimeMillis() - maxAgeMillis
+        directory.listFiles()?.forEach { file ->
+            if (file.isFile && file.lastModified() < expirationTime) {
+                file.delete()
+            }
+        }
+    }
+
+    private fun String.sha256(): String {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(toByteArray())
+        return CharArray(bytes.size * 2).also { result ->
+            bytes.forEachIndexed { index, byte ->
+                val value = byte.toInt() and 0xff
+                result[index * 2] = hexDigits[value ushr 4]
+                result[index * 2 + 1] = hexDigits[value and 0x0f]
+            }
+        }.concatToString()
+    }
+
+    private fun String.isSha256(): Boolean =
+        length == HASH_LENGTH && all { it in '0'..'9' || it in 'a'..'f' }
+}

app/src/main/java/com/eterultimate/eteruee/ui/pages/webview/WebViewPage.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -37,13 +38,14 @@ import me.rerere.hugeicons.stroke.MoreVertical
 import com.eterultimate.eteruee.ui.components.nav.BackButton
 import com.eterultimate.eteruee.ui.components.webview.WebView
 import com.eterultimate.eteruee.ui.components.webview.disableLocalFileAccess
+import com.eterultimate.eteruee.ui.components.webview.WebViewContentCache
 import com.eterultimate.eteruee.ui.components.webview.rememberWebViewState
 import com.eterultimate.eteruee.ui.theme.JetbrainsMono
-import com.eterultimate.eteruee.utils.base64Decode
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WebViewPage(url: String, content: String) {
+fun WebViewPage(url: String, contentId: String) {
+    val context = LocalContext.current
     val uriHandler = LocalUriHandler.current
     val state = if (url.isNotEmpty()) {
         rememberWebViewState(
@@ -54,8 +56,11 @@ fun WebViewPage(url: String, content: String) {
                 allowContentAccess = true
             })
     } else {
+        val content = remember(contentId) {
+            WebViewContentCache.load(context.cacheDir, contentId).orEmpty()
+        }
         rememberWebViewState(
-            data = content.base64Decode(),
+            data = content,
             baseUrl = "https://eteruee.local",
             mimeType = "text/html",
             settings = {

app/src/test/java/com/eterultimate/eteruee/ui/components/webview/WebViewContentCacheTest.kt
@@ -1,0 +1,38 @@
+package com.eterultimate.eteruee.ui.components.webview
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class WebViewContentCacheTest {
+    @get:Rule
+    val temporaryFolder = TemporaryFolder()
+
+    @Test
+    fun `stores large content outside navigation state`() {
+        val content = "<html>${"preview".repeat(50_000)}</html>"
+
+        val id = WebViewContentCache.store(temporaryFolder.root, content)
+
+        assertEquals(64, id.length)
+        assertEquals(content, WebViewContentCache.load(temporaryFolder.root, id))
+    }
+
+    @Test
+    fun `reuses the same cache entry for identical content`() {
+        val content = "<html>preview</html>"
+
+        val firstId = WebViewContentCache.store(temporaryFolder.root, content)
+        val secondId = WebViewContentCache.store(temporaryFolder.root, content)
+
+        assertEquals(firstId, secondId)
+        assertEquals(1, temporaryFolder.root.resolve("webview_content").listFiles()?.size)
+    }
+
+    @Test
+    fun `rejects invalid cache ids`() {
+        assertNull(WebViewContentCache.load(temporaryFolder.root, "../content"))
+    }
+}
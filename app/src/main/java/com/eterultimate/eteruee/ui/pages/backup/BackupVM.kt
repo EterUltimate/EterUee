rikkahub/rikkahub

feat: 支持chatbox v2备份导入
close #1799

app/src/main/java/me/rerere/rikkahub/ui/pages/backup/BackupVM.kt
@@ -1,15 +1,20 @@
 package me.rerere.rikkahub.ui.pages.backup
 
 import android.util.Log
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.rerere.rikkahub.data.datastore.Settings
 import me.rerere.rikkahub.data.datastore.SettingsStore
 import me.rerere.rikkahub.data.datastore.WebDavConfig
+import me.rerere.rikkahub.data.files.FilesManager
+import me.rerere.rikkahub.data.files.saveUploadFromBytes
 import me.rerere.rikkahub.data.repository.ConversationRepository
 import me.rerere.rikkahub.data.sync.importer.ChatboxImporter
 import me.rerere.rikkahub.data.sync.importer.CherryStudioProviderImporter
@@ -27,6 +32,7 @@ class BackupVM(
     private val webDavSync: WebDavSync,
     private val s3Sync: S3Sync,
     private val conversationRepository: ConversationRepository,
+    private val filesManager: FilesManager,
 ) : ViewModel() {
     val settings = settingsStore.settingsFlow.stateIn(
         scope = viewModelScope,
@@ -102,28 +108,40 @@ class BackupVM(
         )
     }
 
-    suspend fun restoreFromChatBox(file: File): ChatboxRestoreResult {
+    suspend fun restoreFromChatBox(file: File): ChatboxRestoreResult = withContext(Dispatchers.IO) {
+        val currentSettings = settings.value
         var importedConversations = 0
         var skippedExistingConversations = 0
         val result = ChatboxImporter.importStreaming(
             file = file,
-            assistantId = settings.value.assistantId,
-            providers = settings.value.providers,
+            assistantId = currentSettings.assistantId,
+            providers = currentSettings.providers,
+            shouldImportConversation = { conversationId ->
+                val exists = conversationRepository.existsConversationById(conversationId)
+                if (exists) skippedExistingConversations++
+                !exists
+            },
+            saveImage = { resource ->
+                val entity = filesManager.saveUploadFromBytes(
+                    bytes = resource.bytes,
+                    displayName = resource.fileName,
+                    mimeType = resource.mimeType,
+                )
+                filesManager.getFile(entity).toUri().toString()
+            },
             onConversation = { conversation ->
-                if (conversationRepository.existsConversationById(conversation.id)) {
-                    skippedExistingConversations++
-                } else {
-                    conversationRepository.insertConversation(conversation)
-                    importedConversations++
-                }
+                conversationRepository.insertConversation(conversation)
+                importedConversations++
             }
         )
 
-        val targetAssistantId = settings.value.assistantId
-        settingsStore.update(
-            settings.value.copy(
-                providers = result.providers + settings.value.providers,
-                assistants = settings.value.assistants.map { assistant ->
+        val targetAssistantId = currentSettings.assistantId
+        settingsStore.update { latestSettings ->
+            latestSettings.copy(
+                providers = result.providers + latestSettings.providers.filterNot { existing ->
+                    result.providers.any { imported -> imported.id == existing.id }
+                },
+                assistants = latestSettings.assistants.map { assistant ->
                     if (result.hasConversationSystemPrompt && assistant.id == targetAssistantId) {
                         assistant.copy(allowConversationSystemPrompt = true)
                     } else {
@@ -131,20 +149,24 @@ class BackupVM(
                     }
                 }
             )
-        )
+        }
 
         Log.i(
             TAG,
             "restoreFromChatBox: import ${result.providers.size} providers, " +
                 "$importedConversations conversations, skip $skippedExistingConversations existing, " +
-                "drop ${result.skippedImageParts} images"
+                "import ${result.importedImageParts} images, drop ${result.skippedImageParts} images, " +
+                "skip ${result.skippedForkMessages} fork messages and ${result.skippedSessions} sessions"
         )
-        return ChatboxRestoreResult(
+        ChatboxRestoreResult(
             importedProviders = result.providers.size,
             importedConversations = importedConversations,
             skippedExistingConversations = skippedExistingConversations,
+            importedImageParts = result.importedImageParts,
             skippedImageParts = result.skippedImageParts,
             skippedEmptyMessages = result.skippedEmptyMessages,
+            skippedForkMessages = result.skippedForkMessages,
+            skippedSessions = result.skippedSessions,
         )
     }
 
@@ -214,6 +236,9 @@ data class ChatboxRestoreResult(
     val importedProviders: Int,
     val importedConversations: Int,
     val skippedExistingConversations: Int,
+    val importedImageParts: Int,
     val skippedImageParts: Int,
     val skippedEmptyMessages: Int,
+    val skippedForkMessages: Int,
+    val skippedSessions: Int,
 )

app/src/test/java/me/rerere/rikkahub/data/sync/importer/ChatboxImporterTest.kt
@@ -1,0 +1,137 @@
+package me.rerere.rikkahub.data.sync.importer
+
+import kotlinx.coroutines.runBlocking
+import me.rerere.ai.ui.UIMessagePart
+import me.rerere.rikkahub.data.model.Conversation
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.uuid.Uuid
+
+class ChatboxImporterTest {
+    @get:Rule
+    val temporaryFolder = TemporaryFolder()
+
+    @Test
+    fun `imports backup v2 providers resources and message forks`() = runBlocking {
+        val backup = temporaryFolder.newFile("chatbox.zip")
+        val imageBytes = byteArrayOf(1, 2, 3, 4)
+        ZipOutputStream(backup.outputStream()).use { zip ->
+            zip.writeEntry(
+                "manifest.json",
+                """
+                {
+                  "format": "chatbox-backup",
+                  "formatVersion": 2,
+                  "data": {"settings": {"path": "settings.json"}},
+                  "sessions": [{
+                    "id": "test-session",
+                    "path": "sessions/test-session/session.json",
+                    "meta": {"name": "Manifest title", "createdAt": 1000, "starred": true}
+                  }],
+                  "resources": [{
+                    "id": "resource-1",
+                    "path": "sessions/test-session/resources/resource-1.png",
+                    "mimeType": "image/png",
+                    "kind": "image",
+                    "originalStorageKeys": ["picture:test"]
+                  }]
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry(
+                "settings.json",
+                """
+                {
+                  "providers": {
+                    "deepseek": {
+                      "apiKey": "test-key",
+                      "models": [{
+                        "modelId": "deepseek-test",
+                        "nickname": "DeepSeek Test",
+                        "capabilities": ["vision", "tool_use", "reasoning"]
+                      }]
+                    }
+                  }
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry(
+                "sessions/test-session/session.json",
+                """
+                {
+                  "id": "test-session",
+                  "name": "Session name",
+                  "threadName": "Thread title",
+                  "settings": {"provider": "deepseek", "modelId": "deepseek-test"},
+                  "messages": [
+                    {"id": "system", "role": "system", "contentParts": [{"type": "text", "text": "Be helpful"}]},
+                    {"id": "user", "role": "user", "timestamp": 2000, "contentParts": [
+                      {"type": "text", "text": "Describe this"},
+                      {"type": "image", "storageKey": "picture:test"}
+                    ]},
+                    {"id": "current", "role": "assistant", "timestamp": 3000, "aiProvider": "deepseek", "contentParts": [
+                      {"type": "text", "text": "Current answer"}
+                    ]}
+                  ],
+                  "messageForksHash": {
+                    "user": {
+                      "position": 1,
+                      "lists": [
+                        {"id": "old-fork", "messages": [{"id": "alternative", "role": "assistant", "timestamp": 2500, "contentParts": [{"type": "text", "text": "Alternative answer"}]}]},
+                        {"id": "current-fork", "messages": []}
+                      ]
+                    }
+                  }
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry("sessions/test-session/resources/resource-1.png", imageBytes)
+        }
+
+        var importedConversation: Conversation? = null
+        var savedImage: ChatboxImageResource? = null
+        val result = ChatboxImporter.importStreaming(
+            file = backup,
+            assistantId = Uuid.random(),
+            providers = emptyList(),
+            saveImage = { resource ->
+                savedImage = resource
+                "file:///imported/resource-1.png"
+            },
+            onConversation = { importedConversation = it },
+        )
+
+        assertEquals(1, result.providers.size)
+        assertEquals(1, result.parsedConversations)
+        assertEquals(1, result.importedImageParts)
+        assertEquals(0, result.skippedImageParts)
+        assertArrayEquals(imageBytes, savedImage?.bytes)
+
+        val conversation = assertNotNull(importedConversation).let { importedConversation!! }
+        assertEquals("Thread title", conversation.title)
+        assertEquals("Be helpful", conversation.customSystemPrompt)
+        assertTrue(conversation.isPinned)
+        assertEquals(2, conversation.messageNodes.size)
+        assertTrue(conversation.messageNodes[0].messages.single().parts[1] is UIMessagePart.Image)
+
+        val assistantNode = conversation.messageNodes[1]
+        assertEquals(2, assistantNode.messages.size)
+        assertEquals(1, assistantNode.selectIndex)
+        assertEquals("Alternative answer", (assistantNode.messages[0].parts.single() as UIMessagePart.Text).text)
+        assertEquals("Current answer", (assistantNode.currentMessage.parts.single() as UIMessagePart.Text).text)
+        assertNotNull(assistantNode.currentMessage.modelId)
+    }
+
+    private fun ZipOutputStream.writeEntry(path: String, bytes: ByteArray) {
+        putNextEntry(ZipEntry(path))
+        write(bytes)
+        closeEntry()
+    }
+}

EterUltimate/EterUee

(cherry picked from commit 7714f2bc24a259d4325e7d489e0df64671e5173b)

app/src/main/java/com/eterultimate/eteruee/ui/pages/backup/BackupVM.kt
@@ -1,14 +1,19 @@
 ﻿package com.eterultimate.eteruee.ui.pages.backup
 
 import android.util.Log
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import com.eterultimate.eteruee.data.datastore.Settings
 import com.eterultimate.eteruee.data.datastore.SettingsStore
+import com.eterultimate.eteruee.data.datastore.WebDavConfig
+import com.eterultimate.eteruee.data.files.FilesManager
 import com.eterultimate.eteruee.data.repository.ConversationRepository
 import com.eterultimate.eteruee.data.sync.importer.ChatboxImporter
 import com.eterultimate.eteruee.data.sync.importer.CherryStudioProviderImporter
@@ -28,6 +33,7 @@ class BackupVM(
     private val s3Sync: S3Sync,
     private val postgresGatewaySync: PostgresGatewaySync,
     private val conversationRepository: ConversationRepository,
+    private val filesManager: FilesManager,
 ) : ViewModel() {
     val settings = settingsStore.settingsFlow.stateIn(
         scope = viewModelScope,
@@ -93,53 +99,65 @@ class BackupVM(
         webDavSync.restoreFromLocalFile(file, settings.value.webDavConfig)
     }
 
-    suspend fun restoreFromChatBox(file: File): ChatboxRestoreResult {
-        val payload = ChatboxImporter.import(
+    suspend fun restoreFromChatBox(file: File): ChatboxRestoreResult = withContext(Dispatchers.IO) {
+        val currentSettings = settings.value
+        var importedConversations = 0
+        var skippedExistingConversations = 0
+        val result = ChatboxImporter.importStreaming(
             file = file,
-            assistantId = settings.value.assistantId,
-            providers = settings.value.providers,
+            assistantId = currentSettings.assistantId,
+            providers = currentSettings.providers,
+            shouldImportConversation = { conversationId ->
+                val exists = conversationRepository.existsConversationById(conversationId)
+                if (exists) skippedExistingConversations++
+                !exists
+            },
+            saveImage = { resource ->
+                val entity = filesManager.saveUploadFromBytes(
+                    bytes = resource.bytes,
+                    displayName = resource.fileName,
+                    mimeType = resource.mimeType,
+                )
+                filesManager.getFile(entity).toUri().toString()
+            },
+            onConversation = { conversation ->
+                conversationRepository.insertConversation(conversation)
+                importedConversations++
+            }
         )
-        val targetAssistantId = settings.value.assistantId
-        val mergedProviders = payload.providers + settings.value.providers
-        val shouldAllowConversationSystemPrompt = payload.conversations.conversations.any {
-            !it.customSystemPrompt.isNullOrBlank()
-        }
-        settingsStore.update(
-            settings.value.copy(
-                providers = mergedProviders,
-                assistants = settings.value.assistants.map { assistant ->
-                    if (shouldAllowConversationSystemPrompt && assistant.id == targetAssistantId) {
+
+        val targetAssistantId = currentSettings.assistantId
+        settingsStore.update { latestSettings ->
+            latestSettings.copy(
+                providers = result.providers + latestSettings.providers.filterNot { existing ->
+                    result.providers.any { imported -> imported.id == existing.id }
+                },
+                assistants = latestSettings.assistants.map { assistant ->
+                    if (result.hasConversationSystemPrompt && assistant.id == targetAssistantId) {
                         assistant.copy(allowConversationSystemPrompt = true)
                     } else {
                         assistant
                     }
                 }
             )
-        )
-
-        var importedConversations = 0
-        var skippedExistingConversations = 0
-        payload.conversations.conversations.forEach { conversation ->
-            if (conversationRepository.existsConversationById(conversation.id)) {
-                skippedExistingConversations++
-            } else {
-                conversationRepository.insertConversation(conversation)
-                importedConversations++
-            }
         }
 
         Log.i(
             TAG,
-            "restoreFromChatBox: import ${payload.providers.size} providers, " +
+            "restoreFromChatBox: import ${result.providers.size} providers, " +
                 "$importedConversations conversations, skip $skippedExistingConversations existing, " +
-                "drop ${payload.conversations.skippedImageParts} images"
+                "import ${result.importedImageParts} images, drop ${result.skippedImageParts} images, " +
+                "skip ${result.skippedForkMessages} fork messages and ${result.skippedSessions} sessions"
         )
-        return ChatboxRestoreResult(
-            importedProviders = payload.providers.size,
+        ChatboxRestoreResult(
+            importedProviders = result.providers.size,
             importedConversations = importedConversations,
             skippedExistingConversations = skippedExistingConversations,
-            skippedImageParts = payload.conversations.skippedImageParts,
-            skippedEmptyMessages = payload.conversations.skippedEmptyMessages,
+            importedImageParts = result.importedImageParts,
+            skippedImageParts = result.skippedImageParts,
+            skippedEmptyMessages = result.skippedEmptyMessages,
+            skippedForkMessages = result.skippedForkMessages,
+            skippedSessions = result.skippedSessions,
         )
     }
 
@@ -218,6 +236,9 @@ data class ChatboxRestoreResult(
     val importedProviders: Int,
     val importedConversations: Int,
     val skippedExistingConversations: Int,
+    val importedImageParts: Int,
     val skippedImageParts: Int,
     val skippedEmptyMessages: Int,
+    val skippedForkMessages: Int,
+    val skippedSessions: Int,
 )

app/src/test/java/com/eterultimate/eteruee/data/sync/importer/ChatboxImporterTest.kt
@@ -1,0 +1,137 @@
+package com.eterultimate.eteruee.data.sync.importer
+
+import kotlinx.coroutines.runBlocking
+import com.eterultimate.eteruee.ai.ui.UIMessagePart
+import com.eterultimate.eteruee.data.model.Conversation
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.uuid.Uuid
+
+class ChatboxImporterTest {
+    @get:Rule
+    val temporaryFolder = TemporaryFolder()
+
+    @Test
+    fun `imports backup v2 providers resources and message forks`() = runBlocking {
+        val backup = temporaryFolder.newFile("chatbox.zip")
+        val imageBytes = byteArrayOf(1, 2, 3, 4)
+        ZipOutputStream(backup.outputStream()).use { zip ->
+            zip.writeEntry(
+                "manifest.json",
+                """
+                {
+                  "format": "chatbox-backup",
+                  "formatVersion": 2,
+                  "data": {"settings": {"path": "settings.json"}},
+                  "sessions": [{
+                    "id": "test-session",
+                    "path": "sessions/test-session/session.json",
+                    "meta": {"name": "Manifest title", "createdAt": 1000, "starred": true}
+                  }],
+                  "resources": [{
+                    "id": "resource-1",
+                    "path": "sessions/test-session/resources/resource-1.png",
+                    "mimeType": "image/png",
+                    "kind": "image",
+                    "originalStorageKeys": ["picture:test"]
+                  }]
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry(
+                "settings.json",
+                """
+                {
+                  "providers": {
+                    "deepseek": {
+                      "apiKey": "test-key",
+                      "models": [{
+                        "modelId": "deepseek-test",
+                        "nickname": "DeepSeek Test",
+                        "capabilities": ["vision", "tool_use", "reasoning"]
+                      }]
+                    }
+                  }
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry(
+                "sessions/test-session/session.json",
+                """
+                {
+                  "id": "test-session",
+                  "name": "Session name",
+                  "threadName": "Thread title",
+                  "settings": {"provider": "deepseek", "modelId": "deepseek-test"},
+                  "messages": [
+                    {"id": "system", "role": "system", "contentParts": [{"type": "text", "text": "Be helpful"}]},
+                    {"id": "user", "role": "user", "timestamp": 2000, "contentParts": [
+                      {"type": "text", "text": "Describe this"},
+                      {"type": "image", "storageKey": "picture:test"}
+                    ]},
+                    {"id": "current", "role": "assistant", "timestamp": 3000, "aiProvider": "deepseek", "contentParts": [
+                      {"type": "text", "text": "Current answer"}
+                    ]}
+                  ],
+                  "messageForksHash": {
+                    "user": {
+                      "position": 1,
+                      "lists": [
+                        {"id": "old-fork", "messages": [{"id": "alternative", "role": "assistant", "timestamp": 2500, "contentParts": [{"type": "text", "text": "Alternative answer"}]}]},
+                        {"id": "current-fork", "messages": []}
+                      ]
+                    }
+                  }
+                }
+                """.trimIndent().encodeToByteArray(),
+            )
+            zip.writeEntry("sessions/test-session/resources/resource-1.png", imageBytes)
+        }
+
+        var importedConversation: Conversation? = null
+        var savedImage: ChatboxImageResource? = null
+        val result = ChatboxImporter.importStreaming(
+            file = backup,
+            assistantId = Uuid.random(),
+            providers = emptyList(),
+            saveImage = { resource ->
+                savedImage = resource
+                "file:///imported/resource-1.png"
+            },
+            onConversation = { importedConversation = it },
+        )
+
+        assertEquals(1, result.providers.size)
+        assertEquals(1, result.parsedConversations)
+        assertEquals(1, result.importedImageParts)
+        assertEquals(0, result.skippedImageParts)
+        assertArrayEquals(imageBytes, savedImage?.bytes)
+
+        val conversation = assertNotNull(importedConversation).let { importedConversation!! }
+        assertEquals("Thread title", conversation.title)
+        assertEquals("Be helpful", conversation.customSystemPrompt)
+        assertTrue(conversation.isPinned)
+        assertEquals(2, conversation.messageNodes.size)
+        assertTrue(conversation.messageNodes[0].messages.single().parts[1] is UIMessagePart.Image)
+
+        val assistantNode = conversation.messageNodes[1]
+        assertEquals(2, assistantNode.messages.size)
+        assertEquals(1, assistantNode.selectIndex)
+        assertEquals("Alternative answer", (assistantNode.messages[0].parts.single() as UIMessagePart.Text).text)
+        assertEquals("Current answer", (assistantNode.currentMessage.parts.single() as UIMessagePart.Text).text)
+        assertNotNull(assistantNode.currentMessage.modelId)
+    }
+
+    private fun ZipOutputStream.writeEntry(path: String, bytes: ByteArray) {
+        putNextEntry(ZipEntry(path))
+        write(bytes)
+        closeEntry()
+    }
+}
rikkahub/rikkahub

fix(mermaid): 内置 mermaid.min.js，修复首次渲染耗时过长 (#1559)
mermaid 此前从 cdn.jsdelivr.net 加载 3.5MB 的 JS，网络较差时首次渲染
需要几十秒白屏，之后靠 WebView 缓存才恢复正常。

改为将 mermaid 11.16.0 打包进 assets，并在 WebViewClient 中拦截
https://rikkahub.local/assets/** 映射到 assets 目录，内嵌 WebView 与
全屏预览页共用同一 baseUrl，均可离线加载。

app/src/main/java/me/rerere/rikkahub/ui/components/richtext/Mermaid.kt
@@ -28,6 +28,8 @@ import me.rerere.hugeicons.stroke.Download01
 import me.rerere.hugeicons.stroke.View
 import me.rerere.rikkahub.R
 import me.rerere.rikkahub.Screen
+import me.rerere.rikkahub.ui.components.webview.WEB_VIEW_ASSET_URL
+import me.rerere.rikkahub.ui.components.webview.WEB_VIEW_BASE_URL
 import me.rerere.rikkahub.ui.components.webview.WebView
 import me.rerere.rikkahub.ui.components.webview.WebViewContentCache
 import me.rerere.rikkahub.ui.components.webview.rememberWebViewState
@@ -92,6 +94,7 @@ fun Mermaid(
 
     val webViewState = rememberWebViewState(
         data = html,
+        baseUrl = WEB_VIEW_BASE_URL,
         mimeType = "text/html",
         encoding = "UTF-8",
         interfaces = mapOf(
@@ -182,7 +185,7 @@ private fun buildMermaidHtml(
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=1024">
-            <script src="https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.min.js"></script>
+            <script src="${WEB_VIEW_ASSET_URL}/html/mermaid.min.js"></script>
             <style>
                 body {
                     margin: 0;

app/src/main/java/me/rerere/rikkahub/ui/components/webview/WebViewLocalAssets.kt
@@ -1,0 +1,56 @@
+package me.rerere.rikkahub.ui.components.webview
+
+import android.content.Context
+import android.net.Uri
+import android.webkit.WebResourceResponse
+
+/**
+ * 应用内 WebView 使用的虚拟域名，用于给 loadDataWithBaseURL 提供一个稳定的 origin。
+ */
+const val WEB_VIEW_BASE_URL = "https://rikkahub.local"
+
+/**
+ * 虚拟域名下的静态资源前缀，会被映射到 app 的 assets 目录。
+ *
+ * 例如 `https://rikkahub.local/assets/html/mermaid.min.js` 对应 `assets/html/mermaid.min.js`，
+ * 这样 mermaid/katex 之类的库可以本地加载，而不用从 CDN 下载（首次加载可能需要几十秒）。
+ */
+const val WEB_VIEW_ASSET_URL = "$WEB_VIEW_BASE_URL/assets"
+
+private const val ASSET_HOST = "rikkahub.local"
+private const val ASSET_PATH_PREFIX = "/assets/"
+
+internal object WebViewLocalAssets {
+    fun intercept(context: Context, uri: Uri): WebResourceResponse? {
+        if (!uri.host.equals(ASSET_HOST, ignoreCase = true)) return null
+        val path = uri.path ?: return null
+        if (!path.startsWith(ASSET_PATH_PREFIX)) return null
+
+        val assetPath = path.removePrefix(ASSET_PATH_PREFIX)
+        if (assetPath.isEmpty() || assetPath.contains("..")) return null
+
+        val mimeType = mimeTypeOf(assetPath)
+        return runCatching {
+            WebResourceResponse(
+                mimeType,
+                if (mimeType.startsWith("text/") || mimeType.endsWith("json") || mimeType.endsWith("xml")) "UTF-8" else null,
+                context.assets.open(assetPath)
+            )
+        }.getOrNull()
+    }
+
+    private fun mimeTypeOf(path: String): String = when (path.substringAfterLast('.', "").lowercase()) {
+        "js", "mjs" -> "text/javascript"
+        "css" -> "text/css"
+        "html", "htm" -> "text/html"
+        "json" -> "application/json"
+        "svg" -> "image/svg+xml"
+        "png" -> "image/png"
+        "jpg", "jpeg" -> "image/jpeg"
+        "webp" -> "image/webp"
+        "woff2" -> "font/woff2"
+        "woff" -> "font/woff"
+        "ttf" -> "font/ttf"
+        else -> "application/octet-stream"
+    }
+}

app/src/main/java/me/rerere/rikkahub/ui/pages/webview/WebViewPage.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import me.rerere.hugeicons.stroke.MoreVertical
 import me.rerere.rikkahub.ui.components.nav.BackButton
+import me.rerere.rikkahub.ui.components.webview.WEB_VIEW_BASE_URL
 import me.rerere.rikkahub.ui.components.webview.WebView
 import me.rerere.rikkahub.ui.components.webview.WebViewContentCache
 import me.rerere.rikkahub.ui.components.webview.rememberWebViewState
@@ -61,7 +62,7 @@ fun WebViewPage(url: String, contentId: String) {
         }
         rememberWebViewState(
             data = content,
-            baseUrl = "https://rikkahub.local",
+            baseUrl = WEB_VIEW_BASE_URL,
             mimeType = "text/html",
             settings = {
                 builtInZoomControls = true

EterUltimate/EterUee

(cherry picked from commit 19e74b9a92d68783eef547bde5dbb83ff09666a4)

app/src/main/java/com/eterultimate/eteruee/ui/components/richtext/Mermaid.kt
@@ -38,6 +38,9 @@ import me.rerere.hugeicons.stroke.Cancel01
 import me.rerere.hugeicons.stroke.Download01
 import me.rerere.hugeicons.stroke.View
 import com.eterultimate.eteruee.R
+import com.eterultimate.eteruee.Screen
+import com.eterultimate.eteruee.ui.components.webview.WEB_VIEW_ASSET_URL
+import com.eterultimate.eteruee.ui.components.webview.WEB_VIEW_BASE_URL
 import com.eterultimate.eteruee.ui.components.webview.WebView
 import com.eterultimate.eteruee.ui.components.webview.rememberWebViewState
 import com.eterultimate.eteruee.ui.context.LocalToaster
@@ -122,6 +125,7 @@ fun Mermaid(
 
     val webViewState = rememberWebViewState(
         data = html,
+        baseUrl = WEB_VIEW_BASE_URL,
         mimeType = "text/html",
         encoding = "UTF-8",
         interfaces = mapOf(
@@ -279,9 +283,8 @@ private fun buildMermaidHtml(
         <html>
         <head>
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=5.0">
-            <title>Mermaid Diagram</title>
-            <script src="https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"></script>
+            <meta name="viewport" content="width=1024">
+            <script src="${WEB_VIEW_ASSET_URL}/html/mermaid.min.js"></script>
             <style>
                 body {
                     margin: 0;

app/src/main/java/com/eterultimate/eteruee/ui/components/webview/WebViewLocalAssets.kt
@@ -1,0 +1,56 @@
+package com.eterultimate.eteruee.ui.components.webview
+
+import android.content.Context
+import android.net.Uri
+import android.webkit.WebResourceResponse
+
+/**
+ * 应用内 WebView 使用的虚拟域名，用于给 loadDataWithBaseURL 提供一个稳定的 origin。
+ */
+const val WEB_VIEW_BASE_URL = "https://eteruee.local"
+
+/**
+ * 虚拟域名下的静态资源前缀，会被映射到 app 的 assets 目录。
+ *
+ * 例如 `https://eteruee.local/assets/html/mermaid.min.js` 对应 `assets/html/mermaid.min.js`，
+ * 这样 mermaid/katex 之类的库可以本地加载，而不用从 CDN 下载（首次加载可能需要几十秒）。
+ */
+const val WEB_VIEW_ASSET_URL = "$WEB_VIEW_BASE_URL/assets"
+
+private const val ASSET_HOST = "rikkahub.local"
+private const val ASSET_PATH_PREFIX = "/assets/"
+
+internal object WebViewLocalAssets {
+    fun intercept(context: Context, uri: Uri): WebResourceResponse? {
+        if (!uri.host.equals(ASSET_HOST, ignoreCase = true)) return null
+        val path = uri.path ?: return null
+        if (!path.startsWith(ASSET_PATH_PREFIX)) return null
+
+        val assetPath = path.removePrefix(ASSET_PATH_PREFIX)
+        if (assetPath.isEmpty() || assetPath.contains("..")) return null
+
+        val mimeType = mimeTypeOf(assetPath)
+        return runCatching {
+            WebResourceResponse(
+                mimeType,
+                if (mimeType.startsWith("text/") || mimeType.endsWith("json") || mimeType.endsWith("xml")) "UTF-8" else null,
+                context.assets.open(assetPath)
+            )
+        }.getOrNull()
+    }
+
+    private fun mimeTypeOf(path: String): String = when (path.substringAfterLast('.', "").lowercase()) {
+        "js", "mjs" -> "text/javascript"
+        "css" -> "text/css"
+        "html", "htm" -> "text/html"
+        "json" -> "application/json"
+        "svg" -> "image/svg+xml"
+        "png" -> "image/png"
+        "jpg", "jpeg" -> "image/jpeg"
+        "webp" -> "image/webp"
+        "woff2" -> "font/woff2"
+        "woff" -> "font/woff"
+        "ttf" -> "font/ttf"
+        else -> "application/octet-stream"
+    }
+}

app/src/main/java/com/eterultimate/eteruee/ui/pages/webview/WebViewPage.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import me.rerere.hugeicons.stroke.MoreVertical
 import com.eterultimate.eteruee.ui.components.nav.BackButton
+import com.eterultimate.eteruee.ui.components.webview.WEB_VIEW_BASE_URL
 import com.eterultimate.eteruee.ui.components.webview.WebView
 import com.eterultimate.eteruee.ui.components.webview.disableLocalFileAccess
 import com.eterultimate.eteruee.ui.components.webview.WebViewContentCache
@@ -61,7 +62,7 @@ fun WebViewPage(url: String, contentId: String) {
         }
         rememberWebViewState(
             data = content,
-            baseUrl = "https://eteruee.local",
+            baseUrl = WEB_VIEW_BASE_URL,
             mimeType = "text/html",
             settings = {
                 builtInZoomControls = true
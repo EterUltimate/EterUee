rikkahub/rikkahub

fix: resolve skills from metadata directories (#1566)

app/src/main/java/me/rerere/rikkahub/data/ai/tools/SkillsTools.kt
@@ -7,13 +7,13 @@ import kotlinx.serialization.json.put
 import me.rerere.ai.core.InputSchema
 import me.rerere.ai.core.Tool
 import me.rerere.ai.ui.UIMessagePart
-import me.rerere.rikkahub.data.files.SkillManager
+import me.rerere.rikkahub.data.files.SkillFrontmatterParser
 import me.rerere.rikkahub.data.files.SkillMetadata
+import me.rerere.rikkahub.data.files.SkillPaths
 
 fun createSkillTools(
     enabledSkills: Set<String>,
     allSkills: List<SkillMetadata>,
-    skillManager: SkillManager,
 ): List<Tool> {
     val available = allSkills.filter { it.name in enabledSkills }
     if (available.isEmpty()) return emptyList()
@@ -61,15 +61,14 @@ fun createSkillTools(
             execute = {
                 val name = it.jsonObject["name"]?.jsonPrimitive?.content
                     ?: error("name is required")
-                if (name !in enabledSkills) {
-                    error("Skill '$name' is not available. Available skills: ${enabledSkills.joinToString()}")
-                }
+                val skill = available.firstOrNull { skill -> skill.name == name }
+                    ?: error("Skill '$name' is not available. Available skills: ${available.joinToString { it.name }}")
                 val path = it.jsonObject["path"]?.jsonPrimitive?.content
                 val content = if (path.isNullOrBlank()) {
-                    skillManager.readSkillBody(name)
-                        ?: error("Skill '$name' not found")
+                    require(skill.skillFile.exists()) { "Skill '$name' not found" }
+                    SkillFrontmatterParser.extractBody(skill.skillFile.readText())
                 } else {
-                    val target = skillManager.resolveSkillFile(name, path)
+                    val target = SkillPaths.resolveSkillFile(skill.skillDir, path)
                         ?: error("Path '$path' is outside the skill directory")
                     require(target.exists()) { "File '$path' not found in skill '$name'" }
                     target.readText()

app/src/test/java/me/rerere/rikkahub/data/ai/tools/SkillsToolsTest.kt
@@ -1,0 +1,48 @@
+package me.rerere.rikkahub.data.ai.tools
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import me.rerere.ai.ui.UIMessagePart
+import me.rerere.rikkahub.data.files.SkillMetadata
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class SkillsToolsTest {
+    @get:Rule
+    val tempFolder = TemporaryFolder()
+
+    @Test
+    fun `use_skill reads metadata directory when display name differs`() = runBlocking {
+        val skillDir = tempFolder.newFolder("directory-name")
+        skillDir.resolve("SKILL.md").writeText(
+            """
+                ---
+                name: Display Name
+                description: Test skill
+                ---
+                Skill instructions
+            """.trimIndent()
+        )
+        val tool = createSkillTools(
+            enabledSkills = setOf("Display Name"),
+            allSkills = listOf(
+                SkillMetadata(
+                    name = "Display Name",
+                    description = "Test skill",
+                    skillDir = skillDir,
+                )
+            ),
+        ).single()
+
+        val result = tool.execute(
+            buildJsonObject {
+                put("name", "Display Name")
+            }
+        )
+
+        assertEquals("Skill instructions", (result.single() as UIMessagePart.Text).text)
+    }
+}

EterUltimate/EterUee

(cherry picked from commit 1428a4b3bdb207f08dfa29f2871563e78d55da20)

app/src/main/java/com/eterultimate/eteruee/data/ai/tools/SkillsTools.kt
@@ -7,13 +7,13 @@ import kotlinx.serialization.json.put
 import com.eterultimate.eteruee.ai.core.InputSchema
 import com.eterultimate.eteruee.ai.core.Tool
 import com.eterultimate.eteruee.ai.ui.UIMessagePart
-import com.eterultimate.eteruee.data.files.SkillManager
+import com.eterultimate.eteruee.data.files.SkillFrontmatterParser
 import com.eterultimate.eteruee.data.files.SkillMetadata
+import com.eterultimate.eteruee.data.files.SkillPaths
 
 fun createSkillTools(
     enabledSkills: Set<String>,
     allSkills: List<SkillMetadata>,
-    skillManager: SkillManager,
 ): List<Tool> {
     val available = allSkills
         .filter { it.name in enabledSkills }
@@ -63,17 +63,15 @@ fun createSkillTools(
             execute = {
                 val name = it.jsonObject["name"]?.jsonPrimitive?.content
                     ?: error("name is required")
-                if (name !in enabledSkills) {
-                    val availableNames = available.map { skill -> skill.name }.sorted()
-                    error("Skill '$name' is not available. Available skills: ${availableNames.joinToString()}")
-                }
+                val skill = available.firstOrNull { it.name == name }
+                    ?: error("Skill '$name' is not enabled. Enabled skills: ${available.joinToString { it.name }}")
                 val path = it.jsonObject["path"]?.jsonPrimitive?.content
                 val content = if (path.isNullOrBlank()) {
-                    skillManager.readSkillBody(name)
-                        ?: error("Skill '$name' not found")
+                    require(skill.skillFile.exists()) { "Skill '$name' not found" }
+                    SkillFrontmatterParser.extractBody(skill.skillFile.readText())
                 } else {
-                    val target = skillManager.resolveSkillFile(name, path)
-                        ?: error("Path '$path' is outside the skill package")
+                    val target = SkillPaths.resolveSkillFile(skill.skillDir, path)
+                        ?: error("Path '$path' is outside the skill directory")
                     require(target.exists()) { "File '$path' not found in skill '$name'" }
                     target.readText()
                 }

app/src/test/java/com/eterultimate/eteruee/data/ai/tools/SkillsToolsTest.kt
@@ -1,0 +1,48 @@
+package com.eterultimate.eteruee.data.ai.tools
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import com.eterultimate.eteruee.ai.ui.UIMessagePart
+import com.eterultimate.eteruee.data.files.SkillMetadata
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class SkillsToolsTest {
+    @get:Rule
+    val tempFolder = TemporaryFolder()
+
+    @Test
+    fun `use_skill reads metadata directory when display name differs`() = runBlocking {
+        val skillDir = tempFolder.newFolder("directory-name")
+        skillDir.resolve("SKILL.md").writeText(
+            """
+                ---
+                name: Display Name
+                description: Test skill
+                ---
+                Skill instructions
+            """.trimIndent()
+        )
+        val tool = createSkillTools(
+            enabledSkills = setOf("Display Name"),
+            allSkills = listOf(
+                SkillMetadata(
+                    name = "Display Name",
+                    description = "Test skill",
+                    skillDir = skillDir,
+                )
+            ),
+        ).single()
+
+        val result = tool.execute(
+            buildJsonObject {
+                put("name", "Display Name")
+            }
+        )
+
+        assertEquals("Skill instructions", (result.single() as UIMessagePart.Text).text)
+    }
+}
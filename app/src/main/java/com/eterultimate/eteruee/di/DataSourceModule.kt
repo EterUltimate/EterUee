rikkahub/rikkahub

refactor(ai): extract translation handler
Move translation model orchestration into a dedicated handler and update dependency injection and consumers.

app/src/main/java/me/rerere/rikkahub/di/DataSourceModule.kt
@@ -18,6 +18,7 @@ import me.rerere.rikkahub.data.ai.AIRequestInterceptor
 import me.rerere.rikkahub.data.ai.RequestLoggingInterceptor
 import me.rerere.rikkahub.data.ai.transformers.AssistantTemplateLoader
 import me.rerere.rikkahub.data.ai.GenerationHandler
+import me.rerere.rikkahub.data.ai.TranslationHandler
 import me.rerere.rikkahub.data.ai.transformers.TemplateTransformer
 import me.rerere.rikkahub.data.api.RikkaHubAPI
 import me.rerere.rikkahub.data.api.SponsorAPI
@@ -163,6 +164,10 @@ val dataSourceModule = module {
             json = get(),
             memoryRepo = get()
         )
+    }
+
+    single {
+        TranslationHandler(providerManager = get())
     }
 
     single<OkHttpClient> {

app/src/main/java/me/rerere/rikkahub/service/ChatService.kt
@@ -47,6 +47,7 @@ import me.rerere.rikkahub.AppScope
 import me.rerere.rikkahub.R
 import me.rerere.rikkahub.data.ai.GenerationChunk
 import me.rerere.rikkahub.data.ai.GenerationHandler
+import me.rerere.rikkahub.data.ai.TranslationHandler
 import me.rerere.rikkahub.data.ai.mcp.McpManager
 import me.rerere.rikkahub.data.ai.tools.createConversationTools
 import me.rerere.rikkahub.data.ai.tools.local.LocalTools
@@ -161,6 +162,7 @@ class ChatService(
     private val conversationRepo: ConversationRepository,
     private val memoryRepository: MemoryRepository,
     private val generationHandler: GenerationHandler,
+    private val translationHandler: TranslationHandler,
     private val templateTransformer: TemplateTransformer,
     private val providerManager: ProviderManager,
     private val localTools: LocalTools,
@@ -1079,7 +1081,7 @@ class ChatService(
                 val loadingText = context.getString(R.string.translating)
                 updateTranslationField(conversationId, message.id, loadingText)
 
-                generationHandler.translateText(
+                translationHandler.translateText(
                     settings = settings,
                     sourceText = messageText,
                     targetLanguage = targetLanguage

app/src/main/java/me/rerere/rikkahub/ui/pages/translator/TranslatorVM.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import me.rerere.rikkahub.data.ai.GenerationHandler
+import me.rerere.rikkahub.data.ai.TranslationHandler
 import me.rerere.rikkahub.data.datastore.Settings
 import me.rerere.rikkahub.data.datastore.SettingsStore
 import java.util.Locale
@@ -18,7 +18,7 @@ private const val TAG = "TranslatorVM"
 
 class TranslatorVM(
     private val settingsStore: SettingsStore,
-    private val generationHandler: GenerationHandler,
+    private val translationHandler: TranslationHandler,
 ) : ViewModel() {
     val settings: StateFlow<Settings> = settingsStore.settingsFlow
         .stateIn(viewModelScope, SharingStarted.Lazily, Settings.dummy())
@@ -72,7 +72,7 @@ class TranslatorVM(
 
         currentJob = viewModelScope.launch {
             runCatching {
-                generationHandler.translateText(
+                translationHandler.translateText(
                     settings = settings.value,
                     sourceText = inputText,
                     targetLanguage = targetLanguage.value

EterUltimate/EterUee

(cherry picked from commit 8f4f1286a1c471ff82f571a3b24f988f7578d30e)

app/src/main/java/com/eterultimate/eteruee/di/DataSourceModule.kt
@@ -21,6 +21,7 @@ import com.eterultimate.eteruee.data.ai.AIRequestInterceptor
 import com.eterultimate.eteruee.data.ai.RequestLoggingInterceptor
 import com.eterultimate.eteruee.data.ai.transformers.AssistantTemplateLoader
 import com.eterultimate.eteruee.data.ai.GenerationHandler
+import com.eterultimate.eteruee.data.ai.TranslationHandler
 import com.eterultimate.eteruee.data.ai.transformers.TemplateTransformer
 import com.eterultimate.eteruee.data.api.EterUeeAPI
 import com.eterultimate.eteruee.data.api.SponsorAPI
@@ -129,6 +130,10 @@ val dataSourceModule = module {
             json = get(),
             memoryRepo = get()
         )
+    }
+
+    single {
+        TranslationHandler(providerManager = get())
     }
 
     single<OkHttpClient> {

app/src/main/java/com/eterultimate/eteruee/service/ChatService.kt
@@ -54,6 +54,7 @@ import com.eterultimate.eteruee.R
 import com.eterultimate.eteruee.RouteActivity
 import com.eterultimate.eteruee.data.ai.GenerationChunk
 import com.eterultimate.eteruee.data.ai.GenerationHandler
+import com.eterultimate.eteruee.data.ai.TranslationHandler
 import com.eterultimate.eteruee.data.ai.mcp.McpManager
 import com.eterultimate.eteruee.data.ai.mcp.mcpDisplayToolName
 import com.eterultimate.eteruee.data.ai.mcp.mcpProviderToolName
@@ -146,6 +147,7 @@ class ChatService(
     private val conversationRepo: ConversationRepository,
     private val memoryRepository: MemoryRepository,
     private val generationHandler: GenerationHandler,
+    private val translationHandler: TranslationHandler,
     private val templateTransformer: TemplateTransformer,
     private val providerManager: ProviderManager,
     private val localTools: LocalTools,
@@ -1131,7 +1133,7 @@ class ChatService(
                 val loadingText = context.getString(R.string.translating)
                 updateTranslationField(conversationId, message.id, loadingText)
 
-                generationHandler.translateText(
+                translationHandler.translateText(
                     settings = settings,
                     sourceText = messageText,
                     targetLanguage = targetLanguage

app/src/main/java/com/eterultimate/eteruee/ui/pages/translator/TranslatorVM.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import com.eterultimate.eteruee.data.ai.GenerationHandler
+import com.eterultimate.eteruee.data.ai.TranslationHandler
 import com.eterultimate.eteruee.data.datastore.Settings
 import com.eterultimate.eteruee.data.datastore.SettingsStore
 import java.util.Locale
@@ -18,7 +18,7 @@ private const val TAG = "TranslatorVM"
 
 class TranslatorVM(
     private val settingsStore: SettingsStore,
-    private val generationHandler: GenerationHandler,
+    private val translationHandler: TranslationHandler,
 ) : ViewModel() {
     val settings: StateFlow<Settings> = settingsStore.settingsFlow
         .stateIn(viewModelScope, SharingStarted.Lazily, Settings.dummy())
@@ -72,7 +72,7 @@ class TranslatorVM(
 
         currentJob = viewModelScope.launch {
             runCatching {
-                generationHandler.translateText(
+                translationHandler.translateText(
                     settings = settings.value,
                     sourceText = inputText,
                     targetLanguage = targetLanguage.value
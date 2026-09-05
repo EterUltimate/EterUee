rikkahub/rikkahub

fix(backup): 使用一致性快照并在启动时安全恢复备份
统一本地、WebDAV 和 S3 的备份归档与恢复逻辑，通过数据库快照包含已提交的 WAL 数据。

恢复时先校验并迁移暂存数据，再于应用初始化前安装，支持中断续作和失败回滚。抽取数据库配置与设置持久化逻辑，并补充备份及恢复测试。

app/src/androidTest/java/me/rerere/rikkahub/data/sync/DatabaseBackupTest.kt
@@ -1,0 +1,91 @@
+package me.rerere.rikkahub.data.sync
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import io.requery.android.database.sqlite.SQLiteDatabase
+import io.requery.android.database.sqlite.SQLiteDatabaseConfiguration
+import me.rerere.rikkahub.data.db.SQLiteConfiguration
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.nio.file.Files
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseBackupTest {
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var directory: File
+
+    @Before fun setUp() {
+        directory = Files.createTempDirectory(context.cacheDir.toPath(), "database-backup-test-").toFile()
+    }
+
+    @After fun tearDown() {
+        directory.deleteRecursively()
+    }
+
+    private fun open(file: File): SQLiteDatabase = SQLiteDatabase.openDatabase(
+        SQLiteConfiguration.configure(context, SQLiteDatabaseConfiguration(file.path, SQLiteDatabase.CREATE_IF_NECESSARY)),
+        null,
+        null,
+    )
+
+    private fun createWalDatabase(file: File): SQLiteDatabase = open(file).apply {
+        enableWriteAheadLogging()
+        query("PRAGMA wal_autocheckpoint=0").use { assertTrue(it.moveToFirst()) }
+        execSQL("CREATE TABLE messages (id INTEGER PRIMARY KEY, text TEXT)")
+        query("PRAGMA wal_checkpoint(TRUNCATE)").use { assertTrue(it.moveToFirst()) }
+        execSQL("INSERT INTO messages(text) VALUES ('committed only in WAL')")
+        assertTrue(File(file.path + "-wal").length() > 0)
+    }
+
+    @Test fun legacyWalIsMergedWithoutShmAndWithoutChangingOriginalArchiveFiles() {
+        val source = File(directory, "source")
+        val staged = File(directory, "rikka_hub")
+        createWalDatabase(source).use {
+            source.copyTo(staged)
+            File(source.path + "-wal").copyTo(File(staged.path + "-wal"))
+        }
+        DatabaseBackup.normalize(context, staged)
+        assertFalse(File(staged.path + "-wal").exists())
+        assertFalse(File(staged.path + "-shm").exists())
+        open(staged).use {
+            assertEquals("committed only in WAL", it.stringForQuery("SELECT text FROM messages", null))
+        }
+    }
+
+    @Test fun snapshotContainsWalCommitsAndFtsTablesAndNeedsNoSidecars() {
+        val source = File(directory, "source")
+        val snapshot = File(directory, "snapshot")
+        createWalDatabase(source).use {
+            it.execSQL("CREATE VIRTUAL TABLE search USING fts5(text, tokenize='simple')")
+            it.execSQL("INSERT INTO search(text) VALUES ('hello backup')")
+            DatabaseBackup.createSnapshot(it, snapshot)
+            // Subsequent writes must not appear in the already-created snapshot.
+            it.execSQL("INSERT INTO messages(text) VALUES ('after snapshot')")
+        }
+        assertFalse(File(snapshot.path + "-wal").exists())
+        assertFalse(File(snapshot.path + "-shm").exists())
+        DatabaseBackup.normalize(context, snapshot)
+        open(snapshot).use {
+            assertEquals(1L, it.longForQuery("SELECT count(*) FROM messages", null))
+            assertEquals(1L, it.longForQuery("SELECT count(*) FROM search WHERE search MATCH 'hello'", null))
+        }
+    }
+
+    @Test fun corruptDatabaseIsRejected() {
+        val file = File(directory, "invalid")
+        file.writeText("not a SQLite database")
+        var failed = false
+        try {
+            DatabaseBackup.normalize(context, file)
+        } catch (_: Exception) {
+            failed = true
+        }
+        assertTrue("Corrupt backup must not be published", failed)
+    }
+}

app/src/main/java/me/rerere/rikkahub/data/sync/PendingRestore.kt
@@ -1,0 +1,168 @@
+package me.rerere.rikkahub.data.sync
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption.ATOMIC_MOVE
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
+import java.util.UUID
+
+/**
+ * Staged restores live outside cache and are installed before any database/settings consumers start.
+ * The journal and atomic moves allow an interrupted installation to resume on the next launch.
+ */
+internal class PendingRestore(
+    private val root: File,
+    private val databaseFile: File,
+    private val filesDir: File,
+) {
+    private val pending get() = File(root, "pending")
+
+    fun createStagingDirectory(): File {
+        check(!pending.exists()) { "A backup restore is already pending. Restart the app first." }
+        check(root.isDirectory || root.mkdirs()) { "Cannot create backup staging directory" }
+        return Files.createTempDirectory(root.toPath(), "preparing-").toFile()
+    }
+
+    fun publish(staging: File) {
+        check(!pending.exists()) { "A backup restore is already pending. Restart the app first." }
+        move(staging, pending)
+    }
+
+    suspend fun apply(restoreSettings: suspend (String) -> Unit): Boolean {
+        // Completed restores must never be replayed, even if cleanup was interrupted.
+        root.listFiles()?.filter {
+            it.name.startsWith("completed-") || it.name.startsWith("preparing-")
+        }?.forEach { it.deleteRecursively() }
+        if (!pending.isDirectory) return false
+
+        val journal = File(pending, "journal.json")
+        val entries = if (journal.exists()) {
+            // An existing journal may describe an interrupted installation; keep it if reading fails.
+            Json.decodeFromString<List<RestoreEntry>>(journal.readText())
+        } else {
+            try {
+                buildEntries().also { writeDurably(journal, Json.encodeToString(it)) }
+            } catch (e: Exception) {
+                // No live files have been changed yet, so this restore can be safely rejected.
+                move(pending, File(root, "failed-${UUID.randomUUID()}"))
+                throw RestoreFailedException(e)
+            }
+        }
+        try {
+            entries.forEach { entry ->
+                val target = targetFile(entry.path)
+                val original = File(pending, "originals/${entry.path}")
+                val source = File(pending, "payload/${entry.path}")
+                if (entry.install && !source.exists()) {
+                    // A previous process already moved this source into place.
+                    check(target.isFile) { "Interrupted restore is missing ${entry.path}" }
+                } else {
+                    if (entry.hadOriginal && !original.exists()) move(target, original)
+                    if (entry.install) move(source, target)
+                }
+            }
+            val settings = File(pending, "settings.json")
+            if (settings.isFile) restoreSettings(settings.readText())
+        } catch (e: Exception) {
+            try {
+                rollback(entries)
+            } catch (rollbackError: Exception) {
+                e.addSuppressed(rollbackError)
+                // Do not start the app with a partially restored database. Keep the journal for retry.
+                throw e
+            }
+            move(pending, File(root, "failed-${UUID.randomUUID()}"))
+            throw RestoreFailedException(e)
+        }
+
+        // Commit by removing the pending name atomically before any live connection can be opened.
+        // If this move fails, startup aborts and the next launch retries the same installation.
+        val completed = File(root, "completed-${UUID.randomUUID()}")
+        move(pending, completed)
+        completed.deleteRecursively()
+        return true
+    }
+
+    private fun buildEntries(): List<RestoreEntry> {
+        val payload = File(pending, "payload")
+        val paths = payload.walkTopDown().filter { it.isFile }.map {
+            it.relativeTo(payload).invariantSeparatorsPath
+        }.toList().sorted()
+        val entries = paths.map { path ->
+            RestoreEntry(path, install = true, hadOriginal = targetFile(path).exists())
+        }.toMutableList()
+        if (paths.contains("database/${databaseFile.name}")) {
+            // The new database is standalone. Keep the old DB's sidecars with the old DB only.
+            for (suffix in listOf("-wal", "-shm", "-journal")) {
+                val path = "database/${databaseFile.name}$suffix"
+                entries.add(0, RestoreEntry(path, install = false, hadOriginal = targetFile(path).exists()))
+            }
+        }
+        return entries
+    }
+
+    private fun rollback(entries: List<RestoreEntry>) {
+        entries.asReversed().forEach { entry ->
+            val target = targetFile(entry.path)
+            val original = File(pending, "originals/${entry.path}")
+            val source = File(pending, "payload/${entry.path}")
+            if (entry.install && !source.exists() && target.exists()) move(target, source)
+            if (original.exists()) move(original, target)
+        }
+    }
+
+    private fun targetFile(path: String): File {
+        return when {
+            path.startsWith("database/") -> {
+                val name = path.removePrefix("database/")
+                require(name in listOf(
+                    databaseFile.name, databaseFile.name + "-wal",
+                    databaseFile.name + "-shm", databaseFile.name + "-journal"
+                )) {
+                    "Invalid restore database path"
+                }
+                File(databaseFile.parentFile, name)
+            }
+            path.startsWith("files/") -> resolveInside(filesDir, path.removePrefix("files/"))
+            else -> error("Invalid restore path: $path")
+        }
+    }
+
+    @Serializable
+    private data class RestoreEntry(val path: String, val install: Boolean, val hadOriginal: Boolean)
+
+    companion object {
+        fun resolveInside(root: File, relativePath: String): File {
+            require(relativePath.isNotBlank() && !relativePath.startsWith('/') &&
+                '\\' !in relativePath && relativePath.split('/').none { it == ".." || it == "." }) {
+                "Invalid backup file path: $relativePath"
+            }
+            val target = File(root, relativePath).canonicalFile
+            require(target.path.startsWith(root.canonicalPath + File.separator)) {
+                "Backup file is outside its target directory"
+            }
+            return target
+        }
+
+        fun writeDurably(file: File, text: String) {
+            val temporary = File(file.parentFile, file.name + ".tmp")
+            FileOutputStream(temporary).use { output ->
+                output.write(text.toByteArray(Charsets.UTF_8))
+                output.fd.sync()
+            }
+            move(temporary, file)
+        }
+
+        private fun move(source: File, target: File) {
+            val parent = requireNotNull(target.parentFile)
+            check(parent.isDirectory || parent.mkdirs()) { "Cannot create restore directory" }
+            Files.move(source.toPath(), target.toPath(), ATOMIC_MOVE, REPLACE_EXISTING)
+        }
+    }
+}
+
+/** Only thrown when original files are intact and the failed import has been isolated. */
+internal class RestoreFailedException(cause: Exception) : Exception("Backup restore failed; original files restored", cause)

EterUltimate/EterUee

移植自 rikkahub 540b9dfaf（-x 标注见下方）：
- 备份创建改用 VACUUM INTO 一致性快照（含 WAL 已提交内容）
- 恢复改为先staging校验（完整性检查+迁移验证）再在下次启动时原子应用
- S3/WebDAV/本地备份统一走 BackupManager，PendingRestore 崩溃安全日志
EterUee 适配：AppDatabaseFactory 使用 BundledSQLiteDriver + MIGRATION_17_18；
DatabaseBackup 完整性检查用 framework SQLite（无 requery/libsimple 依赖）；
FTS 为 unicode61；暂无字体目录，不含 FONTS 备份。
(cherry picked from commit 540b9dfaf5527676d9443482eb44c9483a44a4951)

app/src/androidTest/java/com/eterultimate/eteruee/data/sync/DatabaseBackupTest.kt
@@ -1,0 +1,89 @@
+package com.eterultimate.eteruee.data.sync
+
+import android.database.sqlite.SQLiteDatabase
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.nio.file.Files
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseBackupTest {
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var directory: File
+
+    @Before fun setUp() {
+        directory = Files.createTempDirectory(context.cacheDir.toPath(), "database-backup-test-").toFile()
+    }
+
+    @After fun tearDown() {
+        directory.deleteRecursively()
+    }
+
+    private fun open(file: File): SQLiteDatabase = SQLiteDatabase.openDatabase(
+        file.path,
+        null,
+        SQLiteDatabase.CREATE_IF_NECESSARY,
+    )
+
+    private fun createWalDatabase(file: File): SQLiteDatabase = open(file).apply {
+        enableWriteAheadLogging()
+        query("PRAGMA wal_autocheckpoint=0").use { assertTrue(it.moveToFirst()) }
+        execSQL("CREATE TABLE messages (id INTEGER PRIMARY KEY, text TEXT)")
+        query("PRAGMA wal_checkpoint(TRUNCATE)").use { assertTrue(it.moveToFirst()) }
+        execSQL("INSERT INTO messages(text) VALUES ('committed only in WAL')")
+        assertTrue(File(file.path + "-wal").length() > 0)
+    }
+
+    @Test fun legacyWalIsMergedWithoutShmAndWithoutChangingOriginalArchiveFiles() {
+        val source = File(directory, "source")
+        val staged = File(directory, "rikka_hub")
+        createWalDatabase(source).use {
+            source.copyTo(staged)
+            File(source.path + "-wal").copyTo(File(staged.path + "-wal"))
+        }
+        DatabaseBackup.normalize(context, staged)
+        assertFalse(File(staged.path + "-wal").exists())
+        assertFalse(File(staged.path + "-shm").exists())
+        open(staged).use {
+            assertEquals("committed only in WAL", it.stringForQuery("SELECT text FROM messages", null))
+        }
+    }
+
+    @Test fun snapshotContainsWalCommitsAndFtsTablesAndNeedsNoSidecars() {
+        val source = File(directory, "source")
+        val snapshot = File(directory, "snapshot")
+        createWalDatabase(source).use {
+            it.execSQL("CREATE VIRTUAL TABLE search USING fts5(text, tokenize='unicode61')")
+            it.execSQL("INSERT INTO search(text) VALUES ('hello backup')")
+            DatabaseBackup.createSnapshot(it, snapshot)
+            // Subsequent writes must not appear in the already-created snapshot.
+            it.execSQL("INSERT INTO messages(text) VALUES ('after snapshot')")
+        }
+        assertFalse(File(snapshot.path + "-wal").exists())
+        assertFalse(File(snapshot.path + "-shm").exists())
+        DatabaseBackup.normalize(context, snapshot)
+        open(snapshot).use {
+            assertEquals(1L, it.longForQuery("SELECT count(*) FROM messages", null))
+            assertEquals(1L, it.longForQuery("SELECT count(*) FROM search WHERE search MATCH 'hello'", null))
+        }
+    }
+
+    @Test fun corruptDatabaseIsRejected() {
+        val file = File(directory, "invalid")
+        file.writeText("not a SQLite database")
+        var failed = false
+        try {
+            DatabaseBackup.normalize(context, file)
+        } catch (_: Exception) {
+            failed = true
+        }
+        assertTrue("Corrupt backup must not be published", failed)
+    }
+}

app/src/main/java/com/eterultimate/eteruee/data/sync/PendingRestore.kt
@@ -1,0 +1,168 @@
+package com.eterultimate.eteruee.data.sync
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption.ATOMIC_MOVE
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
+import java.util.UUID
+
+/**
+ * Staged restores live outside cache and are installed before any database/settings consumers start.
+ * The journal and atomic moves allow an interrupted installation to resume on the next launch.
+ */
+internal class PendingRestore(
+    private val root: File,
+    private val databaseFile: File,
+    private val filesDir: File,
+) {
+    private val pending get() = File(root, "pending")
+
+    fun createStagingDirectory(): File {
+        check(!pending.exists()) { "A backup restore is already pending. Restart the app first." }
+        check(root.isDirectory || root.mkdirs()) { "Cannot create backup staging directory" }
+        return Files.createTempDirectory(root.toPath(), "preparing-").toFile()
+    }
+
+    fun publish(staging: File) {
+        check(!pending.exists()) { "A backup restore is already pending. Restart the app first." }
+        move(staging, pending)
+    }
+
+    suspend fun apply(restoreSettings: suspend (String) -> Unit): Boolean {
+        // Completed restores must never be replayed, even if cleanup was interrupted.
+        root.listFiles()?.filter {
+            it.name.startsWith("completed-") || it.name.startsWith("preparing-")
+        }?.forEach { it.deleteRecursively() }
+        if (!pending.isDirectory) return false
+
+        val journal = File(pending, "journal.json")
+        val entries = if (journal.exists()) {
+            // An existing journal may describe an interrupted installation; keep it if reading fails.
+            Json.decodeFromString<List<RestoreEntry>>(journal.readText())
+        } else {
+            try {
+                buildEntries().also { writeDurably(journal, Json.encodeToString(it)) }
+            } catch (e: Exception) {
+                // No live files have been changed yet, so this restore can be safely rejected.
+                move(pending, File(root, "failed-${UUID.randomUUID()}"))
+                throw RestoreFailedException(e)
+            }
+        }
+        try {
+            entries.forEach { entry ->
+                val target = targetFile(entry.path)
+                val original = File(pending, "originals/${entry.path}")
+                val source = File(pending, "payload/${entry.path}")
+                if (entry.install && !source.exists()) {
+                    // A previous process already moved this source into place.
+                    check(target.isFile) { "Interrupted restore is missing ${entry.path}" }
+                } else {
+                    if (entry.hadOriginal && !original.exists()) move(target, original)
+                    if (entry.install) move(source, target)
+                }
+            }
+            val settings = File(pending, "settings.json")
+            if (settings.isFile) restoreSettings(settings.readText())
+        } catch (e: Exception) {
+            try {
+                rollback(entries)
+            } catch (rollbackError: Exception) {
+                e.addSuppressed(rollbackError)
+                // Do not start the app with a partially restored database. Keep the journal for retry.
+                throw e
+            }
+            move(pending, File(root, "failed-${UUID.randomUUID()}"))
+            throw RestoreFailedException(e)
+        }
+
+        // Commit by removing the pending name atomically before any live connection can be opened.
+        // If this move fails, startup aborts and the next launch retries the same installation.
+        val completed = File(root, "completed-${UUID.randomUUID()}")
+        move(pending, completed)
+        completed.deleteRecursively()
+        return true
+    }
+
+    private fun buildEntries(): List<RestoreEntry> {
+        val payload = File(pending, "payload")
+        val paths = payload.walkTopDown().filter { it.isFile }.map {
+            it.relativeTo(payload).invariantSeparatorsPath
+        }.toList().sorted()
+        val entries = paths.map { path ->
+            RestoreEntry(path, install = true, hadOriginal = targetFile(path).exists())
+        }.toMutableList()
+        if (paths.contains("database/${databaseFile.name}")) {
+            // The new database is standalone. Keep the old DB's sidecars with the old DB only.
+            for (suffix in listOf("-wal", "-shm", "-journal")) {
+                val path = "database/${databaseFile.name}$suffix"
+                entries.add(0, RestoreEntry(path, install = false, hadOriginal = targetFile(path).exists()))
+            }
+        }
+        return entries
+    }
+
+    private fun rollback(entries: List<RestoreEntry>) {
+        entries.asReversed().forEach { entry ->
+            val target = targetFile(entry.path)
+            val original = File(pending, "originals/${entry.path}")
+            val source = File(pending, "payload/${entry.path}")
+            if (entry.install && !source.exists() && target.exists()) move(target, source)
+            if (original.exists()) move(original, target)
+        }
+    }
+
+    private fun targetFile(path: String): File {
+        return when {
+            path.startsWith("database/") -> {
+                val name = path.removePrefix("database/")
+                require(name in listOf(
+                    databaseFile.name, databaseFile.name + "-wal",
+                    databaseFile.name + "-shm", databaseFile.name + "-journal"
+                )) {
+                    "Invalid restore database path"
+                }
+                File(databaseFile.parentFile, name)
+            }
+            path.startsWith("files/") -> resolveInside(filesDir, path.removePrefix("files/"))
+            else -> error("Invalid restore path: $path")
+        }
+    }
+
+    @Serializable
+    private data class RestoreEntry(val path: String, val install: Boolean, val hadOriginal: Boolean)
+
+    companion object {
+        fun resolveInside(root: File, relativePath: String): File {
+            require(relativePath.isNotBlank() && !relativePath.startsWith('/') &&
+                '\\' !in relativePath && relativePath.split('/').none { it == ".." || it == "." }) {
+                "Invalid backup file path: $relativePath"
+            }
+            val target = File(root, relativePath).canonicalFile
+            require(target.path.startsWith(root.canonicalPath + File.separator)) {
+                "Backup file is outside its target directory"
+            }
+            return target
+        }
+
+        fun writeDurably(file: File, text: String) {
+            val temporary = File(file.parentFile, file.name + ".tmp")
+            FileOutputStream(temporary).use { output ->
+                output.write(text.toByteArray(Charsets.UTF_8))
+                output.fd.sync()
+            }
+            move(temporary, file)
+        }
+
+        private fun move(source: File, target: File) {
+            val parent = requireNotNull(target.parentFile)
+            check(parent.isDirectory || parent.mkdirs()) { "Cannot create restore directory" }
+            Files.move(source.toPath(), target.toPath(), ATOMIC_MOVE, REPLACE_EXISTING)
+        }
+    }
+}
+
+/** Only thrown when original files are intact and the failed import has been isolated. */
+internal class RestoreFailedException(cause: Exception) : Exception("Backup restore failed; original files restored", cause)